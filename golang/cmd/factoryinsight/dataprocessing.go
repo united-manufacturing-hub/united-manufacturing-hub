d526236171cecec5c4dfe4010f037f58d8c16482
Jeremy Theocharis <Jeremy.Theocharis@industrial-analytics.net>
2021-01-14 19:52:12 +0100

    Test whether staging pipeline works

--- golang/cmd/factoryinsight/dataprocessing.go
+++ golang/cmd/factoryinsight/dataprocessing.go
@@ -1660,6 +1660,8 @@ func automaticallyIdentifyChangeovers(parentSpan opentracing.Span, stateArray []
 		defer span.Finish()
 	}
 
+	zap.S().Infof("automaticallyIdentifyChangeovers loaded")
+
 	// Loop through all datapoints
 	for index, dataPoint := range stateArray {
 
