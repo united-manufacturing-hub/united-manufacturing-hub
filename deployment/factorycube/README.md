4072c9e2d656c8757f70701bbc181ba6fd1f3add
Jeremy Theocharis <jeremy.theocharis@industrial-analytics.net>
2021-02-25 22:05:08 +0100

    temp

--- deployment/factorycube/README.md
+++ deployment/factorycube/README.md
@@ -6,6 +6,8 @@
 You need a HTTP server running in your local network containing the configuration files. You can setup one with
 `docker run -it -v "C:\git\united-manufacturing-hub\deployment\factorycube\webserver:/usr/share/nginx/html:ro" nginx`
 
+tar -czvf factorycube-helm.tar.gz ./factorycube-helm
+
 ### During installation
 
-Enter following cloud-init script http://172.21.9.175/template.yaml
+Enter following cloud-init script http://172.21.9.175/configs/SERIAL_NUMBER.yaml
--- deployment/factorycube/webserver/init_stack.sh
+++ deployment/factorycube/webserver/init_stack.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
+echo "init_stack.sh: Sleeping 20 seconds"
+sleep 20
+
 # Install Helm
 
 # TODO: check whether helm already exists
+echo "Installing Helm..."
 export VERIFY_CHECKSUM=false 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 
 chmod 700 get_helm.sh && ./get_helm.sh
-
-# Remove previous version
-helm uninstall factorycube-helm --kubeconfig /etc/rancher/k3s/k3s.yaml
-rm -R /home/rancher/factorycube-helm
+echo "Helm installed!"
 
 # Download factorycube-helm
 # TODO: replace with github link once published
+echo "Get new version of factorycube-helm..."
 wget http://172.21.9.175/factorycube-helm.tar.gz
 tar -xvf factorycube-helm.tar.gz -C /home/rancher
 rm factorycube-helm.tar.gz
+echo "Got new version of factorycube-helm!"
+
+# Creating MQTT secrets
+echo "Creating MQTT secrets..."
+kubectl create secret generic vernemq-certificates-secret --from-file=tls.crt=/home/rancher/ssl_certs/tls.crt --from-file=tls.key=/home/rancher/ssl_certs/tls.key --from-file=ca.crt=/home/rancher/ssl_certs/ca.crt
+echo "MQTT secrets created!"
 
 # Install factorycube-helm
-# TODO: currently failing first time as /etc/rancher/k3s/k3s.yaml does not exist yet
+echo "Installing factorycube-helm..."
 helm install factorycube-helm /home/rancher/factorycube-helm --kubeconfig /etc/rancher/k3s/k3s.yaml
+echo "factorycube-helm installed!"
