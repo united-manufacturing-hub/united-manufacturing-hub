5c29bb910d9e07de16c344a7071f23dbfddf1118
Ferdinand Linnenberg <fl@umh.app>
2021-07-27 17:28:18 +0200

    feat: removed old grafana plugins folder

--- grafana-plugins/ia-factoryinsight-datasource/src/module.ts
+++
@@ -1,9 +0,0 @@
-import { DataSourcePlugin } from '@grafana/data';
-import { DataSource } from './DataSource';
-import { ConfigEditor } from './ConfigEditor';
-import { QueryEditor } from './QueryEditor';
-import { JSONQuery, JSONQueryOptions } from './types';
-
-export const plugin = new DataSourcePlugin<DataSource, JSONQuery, JSONQueryOptions>(DataSource)
-  .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditor);
