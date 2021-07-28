8c8265be095ac80e1f01acd80ee8231a1ab6837a
felixmhofmann <f.hofmann@oliva-advisory.de>
2021-07-28 11:09:01 +0200

    Version läuft auf lokaler Umgebung - Print durch Logging ersetzt -

--- mqtt-to-blob/src/main.py
+++ mqtt-to-blob/src/main.py
@@ -14,61 +14,64 @@ import time
 #    config = json.load(config_file)
 
 broker_url = os.environ['BROKER_URL']
-print(broker_url)
 broker_port = int(os.environ['BROKER_PORT'])
 minio_url = os.environ['MINIO_URL']
 minio_access_key = os.environ['MINIO_ACCESS_KEY']
 minio_secret = os.environ['MINIO_SECRET_KEY']
-#config = config["Europe/Vienna"]
+bucket_name = os.environ['BUCKET_NAME']
 topic = os.environ['TOPIC']
 input_var = ""
 timezone = pytz.timezone('Europe/Vienna')
 logging.basicConfig(level=logging.DEBUG)
-timestamp = ""
+
+# Create and configure logger
+logging.basicConfig(filename="newfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+# Creating an object
+logger = logging.getLogger()
+#Setting the threshold of logger to DEBUG
+logger.setLevel(logging.DEBUG)
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected With Result Code " + str(rc))
+    logging.info("Connected With Result Code " + str(rc))
 
 def on_message(client, userdata, message):
-    print(message.payload)
     input_var = json.loads(message.payload)
-    timestamp = datetime.datetime.fromtimestamp(int(input_var["timestamp_ms"])/1000.0, timezone).strftime("%Y%m%d_%H-%M-%S-%f")
+    #changed from timestamp to imageID. If the file should be named by the timestamp then use this line instead of the uid.
+    #timestamp = datetime.datetime.fromtimestamp(int(input_var["timestamp_ms"])/1000.0, timezone).strftime("%Y%m%d_%H-%M-%S-%f")
     im = input_var["image"]
+    uid = input_var["imageUID"]
     im_bytes = base64.b64decode(im["image_bytes"])
-    im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+    im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
     img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
-    img_saver = cv2.imwrite("./images/"+timestamp+".jpg", img)
+    img_saver = cv2.imwrite("./images/"+uid+".jpg", img)
     if img_saver==True:
-        print("saved")
+        logger.info("saved")
     else:
-        print("failed to save")
+        logger.debug("failed to save image to cache")
     client = Minio(
         minio_url,
         access_key=minio_access_key,
         secret_key=minio_secret
     )
-    found = client.bucket_exists("umhtest")
+    found = client.bucket_exists(bucket_name)
     if not found:
-        client.make_bucket("umhtest")
+        client.make_bucket(bucket_name)
     else:
-        print("Bucket 'umhtest' already exists")
-    # Upload 'file_path' as object name
-    # Add 'file' to bucket 'umhtest'.
+        logger.info("Bucket bucket_name already exists")
     client.fput_object(
-        "umhtest", timestamp+".jpg", "./images/" + timestamp + ".jpg"
+        bucket_name, uid +".jpg", "./images/" + uid + ".jpg"
     )
-    print(
-        "'test_pic' is successfully uploaded as "
-        "object 'photo' to bucket 'umhtest'."
-    )
-    if os.path.exists("./images/" + timestamp + ".jpg"):
-        os.remove("./images/" + timestamp + ".jpg")
-        print("file has been deleted")
+    logger.info("Successfully uploaded")
+
+    if os.path.exists("./images/" + uid + ".jpg"):
+        os.remove("./images/" + uid + ".jpg")
+        logger.info("file has been deleted")
     else:
-        print("The file does not exist")
+        logger.info("The file does not exist")
 
 if __name__ == "__main__":
-    print(broker_port)
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
