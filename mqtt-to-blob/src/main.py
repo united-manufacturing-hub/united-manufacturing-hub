8c1227d41f0a9e9c4be7e8378fa24ad4e30e85ea
felixmhofmann <f.hofmann@oliva-advisory.de>
2021-07-28 16:24:22 +0200

    all changes as discussed

--- mqtt-to-blob/src/main.py
+++ mqtt-to-blob/src/main.py
@@ -5,11 +5,10 @@ import numpy as np
 import os
 import base64
 import json
-import datetime
-import pytz
 import cv2
 
 #Settig up the env variables, see index.md for further explanation
+LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'INFO')
 broker_url = os.environ['BROKER_URL']
 broker_port = int(os.environ['BROKER_PORT'])
 minio_url = os.environ['MINIO_URL']
@@ -19,21 +18,7 @@ bucket_name = os.environ['BUCKET_NAME']
 topic = os.environ['TOPIC']
 image_uid = os.environ['IMAGE_UID']
 image_bytes = os.environ['IMAGE_BYTES']
-LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'INFO')
-
 input_var = ""
-#timezone = pytz.timezone('Europe/Vienna')
-
-#Creation of a logger to log possible end messages to quicker find bugs.
-logging.basicConfig(level=logging.DEBUG) #Define the level after which the code will write in the log.
-# Create and configure logger
-logging.basicConfig(filename="logfile.log",
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
-# Creating an object
-logger = logging.getLogger()
-#Setting the threshold of logger to DEBUG
-logger.setLevel(logging.DEBUG)
 
 #Connects to the mqtt client. If you want to be sure that the connection attempt was successful then see the logs.
 #This function will return the return code (rc) 0 if connected successfully.
@@ -43,13 +28,10 @@ logger.setLevel(logging.DEBUG)
 def on_connect(client, userdata, flags, rc):
     logging.info("Connected With Result Code " + str(rc))
 
-
 #Message is an object and the payload property contains the message data which is binary data.
 
 def on_message(client, userdata, message):
     input_var = json.loads(message.payload)
-    #changed from timestamp to imageID. If the file should be named by the timestamp then use this line instead of the uid.
-    #timestamp = datetime.datetime.fromtimestamp(int(input_var["timestamp_ms"])/1000.0, timezone).strftime("%Y%m%d_%H-%M-%S-%f")
     im = input_var["image"]
     uid = input_var[image_uid]
     im_bytes = base64.b64decode(im[image_bytes])
@@ -57,9 +39,9 @@ def on_message(client, userdata, message):
     img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
     img_saver = cv2.imwrite("./images/"+uid+".jpg", img)
     if img_saver==True:
-        logger.info("saved")
+        logging.info("saved")
     else:
-        logger.debug("failed to save image to cache")
+        logging.debug("failed to save image to cache")
     client = Minio(
         minio_url,
         access_key=minio_access_key,
@@ -69,19 +51,31 @@ def on_message(client, userdata, message):
     if not found:
         client.make_bucket(bucket_name)
     else:
-        logger.info("Bucket already exists")
+        logging.info("Bucket already exists")
     client.fput_object(
         bucket_name, uid +".jpg", "./images/" + uid + ".jpg"
     )
-    logger.info("Successfully uploaded")
+    logging.info("Successfully uploaded")
 
     if os.path.exists("./images/" + uid + ".jpg"):
         os.remove("./images/" + uid + ".jpg")
-        logger.info("file has been deleted")
+        logging.info("file has been deleted")
     else:
-        logger.info("The file does not exist")
+        logging.info("The file does not exist")
 
 if __name__ == "__main__":
+
+    if LOGGING_LEVEL == "DEBUG":
+        logging.basicConfig(level=logging.DEBUG)
+    elif LOGGING_LEVEL == "INFO":
+        logging.basicConfig(level=logging.INFO)
+    elif LOGGING_LEVEL == "WARNING":
+        logging.basicConfig(level=logging.WARNING)
+    elif LOGGING_LEVEL == "ERROR":
+        logging.basicConfig(level=logging.ERROR)
+    elif LOGGING_LEVEL == "CRITICAL":
+        logging.basicConfig(level=logging.CRITICAL)
+
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
