69724fa071b965901745622cec61289a86182ae5
JeremyTheocharis <jt@umh.app>
2021-07-28 20:47:11 +0200

    Fixed potential bug and formatted

--- mqtt-to-blob/src/main.py
+++ mqtt-to-blob/src/main.py
@@ -7,7 +7,7 @@ import base64
 import json
 import cv2
 
-#Settig up the env variables, see index.md for further explanation
+# Settig up the env variables, see index.md for further explanation
 LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'INFO')
 broker_url = os.environ['BROKER_URL']
 broker_port = int(os.environ['BROKER_PORT'])
@@ -20,15 +20,26 @@ image_uid = os.environ['IMAGE_UID']
 image_bytes = os.environ['IMAGE_BYTES']
 input_var = ""
 
-#Connects to the mqtt client. If you want to be sure that the connection attempt was successful then see the logs.
-#This function will return the return code (rc) 0 if connected successfully.
-#The following values are possible: 0 - Connection successful; 1 - Connection refused – incorrect protocol version; 2 - Connection refused – invalid client identifier;
-#3 - Connection refused – server unavailable; 4 - Connection refused – bad username or password; 5 - Connection refused – not authorised; 6-255 - Currently unused.
+# Connects to the mqtt client.
+# If you want to be sure that the connection attempt was successful,
+# then see the logs.
+# This function will return the return code (rc) 0 if connected successfully.
+# The following values are possible:
+# 0 - Connection successful;
+# 1 - Connection refused – incorrect protocol version;
+# 2 - Connection refused – invalid client identifier;
+# 3 - Connection refused – server unavailable;
+# 4 - Connection refused – bad username or password;
+# 5 - Connection refused – not authorised;
+# 6-255 - Currently unused.
+
 
 def on_connect(client, userdata, flags, rc):
     logging.info("Connected With Result Code " + str(rc))
 
-#Message is an object and the payload property contains the message data which is binary data.
+# Message is an object and the payload property contains
+# the message data which is binary data.
+
 
 def on_message(client, userdata, message):
     input_var = json.loads(message.payload)
@@ -38,22 +49,27 @@ def on_message(client, userdata, message):
     im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
     img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
     img_saver = cv2.imwrite("./images/"+uid+".jpg", img)
-    if img_saver==True:
+
+    if img_saver:
         logging.info("saved")
     else:
         logging.debug("failed to save image to cache")
+
     client = Minio(
         minio_url,
         access_key=minio_access_key,
         secret_key=minio_secret
     )
+
     found = client.bucket_exists(bucket_name)
+
     if not found:
         client.make_bucket(bucket_name)
     else:
         logging.info("Bucket already exists")
+
     client.fput_object(
-        bucket_name, uid +".jpg", "./images/" + uid + ".jpg"
+        bucket_name, uid + ".jpg", "./images/" + uid + ".jpg"
     )
     logging.info("Successfully uploaded")
 
@@ -63,8 +79,8 @@ def on_message(client, userdata, message):
     else:
         logging.info("The file does not exist")
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     if LOGGING_LEVEL == "DEBUG":
         logging.basicConfig(level=logging.DEBUG)
     elif LOGGING_LEVEL == "INFO":
@@ -76,9 +92,9 @@ if __name__ == "__main__":
     elif LOGGING_LEVEL == "CRITICAL":
         logging.basicConfig(level=logging.CRITICAL)
 
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(broker_url, broker_port)
-    client.subscribe(topic, qos=0)
-    client.loop_forever()
+    global_client = mqtt.Client()
+    global_client.on_connect = on_connect
+    global_client.on_message = on_message
+    global_client.connect(broker_url, broker_port)
+    global_client.subscribe(topic, qos=0)
+    global_client.loop_forever()
