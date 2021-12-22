cf2f831c948c15b2e5c6399f51b2b1297a87596a
larswitte <lars.witte@rwth-aachen.de>
2021-12-22 17:12:20 +0100

    went from http.PostForm to http.NewRequest

--- golang/cmd/sensorconnect/iolinkDeviceFinder.go
+++ golang/cmd/sensorconnect/iolinkDeviceFinder.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
+	"bytes"
 	"encoding/binary"
-	"strconv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
 )
 
-
 // Structs for parsing response to discover all IO-Link Master Devices
-type DiscoverResponseFromDevice struct{
-	cid int `xml:"ProcessData,attr"`
+type DiscoverResponseFromDevice struct {
+	cid  int  `xml:"ProcessData,attr"`
 	data Data `xml:"data"`
 }
 
 type Data struct {
-	_deviceinfo_serialnumber_  StringDataPoint `xml:"/deviceinfo/serialnumber/"`//in xml with / instead of _
-	_deviceinfo_productcode_ StringDataPoint `xml:"/deviceinfo/productcode/"`//in xml with / instead of _
+	_deviceinfo_serialnumber_ StringDataPoint `xml:"/deviceinfo/serialnumber/"` //in xml with / instead of _
+	_deviceinfo_productcode_  StringDataPoint `xml:"/deviceinfo/productcode/"`  //in xml with / instead of _
 }
 
 type StringDataPoint struct {
-	code int `xml:"code,attr"`
+	code int    `xml:"code,attr"`
 	data string `xml:"data,attr"`
 }
 
-
 // Struct for relevant information of already discovered devices
-type DiscoveredDeviceInformation struct{
-	productCode string
+type DiscoveredDeviceInformation struct {
+	productCode  string
 	serialNumber string
-	url string
+	url          string
 }
 
-func discoverDevices(discoveredDevices []DiscoveredDeviceInformation, ipRange string)(discoveredDevices []DiscoveredDeviceInformation){
+func discoverDevices(discoveredDevices []DiscoveredDeviceInformation, ipRange string) []DiscoveredDeviceInformation {
 	// Payload to send to the gateways
-    payload := `{
+	var payload = []byte(`{
         "code":"request",
-        "cid":-1,
+        "cid":23,
         "adr":"/getdatamulti",
         "data":{
             "datatosend":[
                 "/deviceinfo/serialnumber/","/deviceinfo/productcode/"]
         }
-    }`
+    }`)
 
 	// CIDR copied from https://stackoverflow.com/questions/60540465/how-to-list-all-ips-in-a-network
 	// convert string to IPNet struct
@@ -71,48 +71,66 @@ func discoverDevices(discoveredDevices []DiscoveredDeviceInformation, ipRange st
 		// converto ip to url
 		url := "http://" + ip.String()
 		//check url address with post request
-		resp, err := http.PostForm(url, payload) //Todo: how to set the timeout = 0.1?
+		/*
+			resp, err := http.PostForm(urlString, url.Values{
+				"code":"request",
+				"cid":24,
+				"adr":"/getdatamulti",
+				"data":{
+					"datatosend":[
+						"/deviceinfo/serialnumber/","/deviceinfo/productcode/"]
+					}
+				}) //Todo: how to set the timeout = 0.1?
+		*/
+
+		fmt.Println("URL:>", url)
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+		req.Header.Set("X-Custom-Header", "myvalue")
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+
 		if err != nil {
 			fmt.Print(err)
 			log.Fatal(err)
 		}
+		if resp.StatusCode != 200 {
+			fmt.Println("Error: Statuscode not euqal 200!")
+			continue
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			// zap.error
+			fmt.Println("ioutil.Readall(resp.Body)  failed")
+			continue
+		}
 		unmarshaledAnswer := DiscoverResponseFromDevice{}
 
 		// Unmarshal file with Unmarshal
-		err := xml.Unmarshal(resp, &unmarshaledAnswer)
+		err = json.Unmarshal(body, &unmarshaledAnswer)
 		if err != nil {
 			panic(err) //Todo change to zap stuff
 		}
 
+		if unmarshaledAnswer.cid != 23 {
+			fmt.Println("Incorrect or missing cid in response. (Should be 23)")
+		}
 		discoveredDeviceInformation := DiscoveredDeviceInformation{}
 		// Insert relevant gained data into DiscoveredDeviceInformation and store in slice
 		discoveredDeviceInformation.productCode = unmarshaledAnswer.data._deviceinfo_productcode_.data
 		discoveredDeviceInformation.serialNumber = unmarshaledAnswer.data._deviceinfo_serialnumber_.data
-		discoveredDeviceInformation.url = url.String()
-		
-		discoveredDevices = append(discoveredDevices, DiscoveredDeviceInformation)
-		return payload, err
+		discoveredDeviceInformation.url = url
 
-
-
+		discoveredDevices = append(discoveredDevices, discoveredDeviceInformation)
 	}
-
-
-    # Iterate over the specified IP space to find connected gateways
-    for ip in ipaddress.IPv4Network(ip_range): #IPv4Network('172.16.0.0/24'):
-        try:
-            url = str(ip)
-            r = requests.post("http://"+url, data=payload, timeout=0.1)
-
-            r.raise_for_status()
-
-            response = json.loads(r.text)
-
-            productCode = response["data"]["/deviceinfo/productcode/"]["data"]
-            serialNumber = response["data"]["/deviceinfo/serialnumber/"]["data"]
-            returnArray.append([url, productCode, serialNumber])
-        except Exception as e:
-            pass
-
-    return returnArray
+	return discoveredDevices
 }
