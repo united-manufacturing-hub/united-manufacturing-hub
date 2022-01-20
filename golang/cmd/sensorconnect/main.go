9a3a611fb79a48a5fafe462477fe6ab56ed515b6
larswitte <lars.witte@rwth-aachen.de>
2022-01-20 08:43:36 +0100

    feat: started writing main.go

--- golang/cmd/sensorconnect/main.go
+++ golang/cmd/sensorconnect/main.go
@@ -1,36 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"reflect" //for reading out type of variable
-	/*
-		"go.uber.org/zap" */)
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var discoveredDeviceInformation []DiscoveredDeviceInformation
 
 func main() {
-	//Read File
-	dat, err := ioutil.ReadFile("C:/Users/LarsWitte/umh_larswitte_repo/sensorconnectRep/united-manufacturing-hub/golang/cmd/sensorconnect/ifm-0002BA-20170227-IODD1.1.xml")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Contents of file:", string(dat))
+	ipRange := "192.168.10.17/32"
 
-	// Unmarshal file
-	var ioDevice IoDevice
-	ioDevice, err = UnmarshalIoddFile(dat)
-	if err != nil {
-		panic(err)
-	}
-	/* 	if err != nil {
-	   		zap.S().Warnf("Invalid IoddFile: %s", err)
-	   	}
-	*/
-	//should give out 698 and int
-	fmt.Println(ioDevice.ProfileBody.DeviceIdentity.DeviceId)
-	fmt.Println(reflect.TypeOf(ioDevice.ProfileBody.DeviceIdentity.DeviceId))
+	tickerSearchForDevices := time.NewTicker(5 * time.Second)
+	defer tickerSearchForDevices.Stop()
+	updaterChan := make(chan struct{})
+	defer close(updaterChan) // close the channel
 
-	//should put out 4 and int
-	fmt.Println(ioDevice.ProfileBody.DeviceFunction.ProcessDataCollection.ProcessData.ProcessDataIn.Datatype.ReccordItem[1].SimpleDatatype.BitLength)
-	fmt.Println(reflect.TypeOf(ioDevice.ProfileBody.DeviceFunction.ProcessDataCollection.ProcessData.ProcessDataIn.Datatype.ReccordItem[1].SimpleDatatype.BitLength))
-	fmt.Println(reflect.TypeOf(dat))
+	go continuousDeviceSearch(tickerSearchForDevices, updaterChan, ipRange)
+
+	select {} // block forever
+}
+func continuousDeviceSearch(ticker *time.Ticker, updaterChan chan struct{}, ipRange string) {
+	for {
+		select {
+		case <-ticker.C:
+			var err error
+			discoveredDeviceInformation, err = DiscoverDevices(ipRange)
+			if err != nil {
+				zap.S().Errorf("DiscoverDevices produced the error: %v", err)
+				continue
+			}
+		case <-updaterChan:
+			return
+		}
+	}
+
 }
