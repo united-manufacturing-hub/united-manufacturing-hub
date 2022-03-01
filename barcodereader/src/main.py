3b21ca413247e2dc6ab3c089970df80e3b0240b4
Alexander Krüger <ak@umh.app>
2022-03-01 15:43:37 -0500

    add support for numbers and . in numpad

--- barcodereader/src/main.py
+++ barcodereader/src/main.py
@@ -66,7 +66,9 @@ scancodes = {
     20: u't', 21: u'y', 22: u'u', 23: u'i', 24: u'o', 25: u'p', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
     30: u'a', 31: u's', 32: u'd', 33: u'f', 34: u'g', 35: u'h', 36: u'j', 37: u'k', 38: u'l', 39: u';',
     40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'z', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
-    50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+    50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 71: u'num 7',
+    72: u'num 8', 73: u'num 9', 75: u'num 4',  76: u'num 5',  77: u'num 6', 79: u'num 1', 80: u'num 2', 81: u'num 3', 82: u'num 0',
+    86: u'num .',100: u'RALT'
 }
 
 capscodes = {
