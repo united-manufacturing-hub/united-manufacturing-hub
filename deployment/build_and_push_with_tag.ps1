2ec6188e4e4f456b63258998236ceb672d1f8f89
Ferdinand Linnenberg <fl@umh.app>
2022-04-20 12:59:59 +0200

    0.9.0 prerelease3 (#979)
    * release: set release version to 0.9.0-prerelease1
    
    * release: Added v0.9.0-prerelease1 release files
    
    * feat: manual build and push for all microservices
    
    * fix: added debug message to processKafkaQueue
    
    * fix: removed incorrect double qouting
    
    * fix: added missing sensorconnect workflow
    
    * fix: added missing mqtt-to-postgresql workflow
    
    * fix: added missing mqtt-to-blob workflow
    
    * feat: datastorage no longer activates mqtt-to-postgresql
    
    * fix: kafka can't handle negative lookaheads
    
    * fix: fixed kafka crashing, while startup, due to livenessProbe
    
    * fix: fixed double shutdown
    
    * release: 00.9.0-prerelease2
    
    * fix: increased vernemq livenessprobe initial delay
    
    * fix: added 1gb limit to kafka-to-postgresql
    
    * fix: set limits for kafka
    
    * fix: set limits for kafka
    
    * feat: changed sensorconnect to only enable kafka/mqtt if enabled and brokers are enabled too
    
    * fix: set mqtt to false as default
    
    * fix: fixed incorrect and comparison
    
    * fix: reduced memory request for kafka and kafka-to-postgres
    
    * release: 0.9.0-prerelease3
    
    * release: 0.9.0-prerelease3
    
    * Update index.yaml
    
    * feat: Changed link to umh repo and removed devel

--- deployment/build_and_push_with_tag.ps1
+++ deployment/build_and_push_with_tag.ps1
@@ -9,15 +9,39 @@ $git_root = git rev-parse --show-toplevel
 $current_location = Get-Location
 
 # Set to gitroot, allowing for relative paths
-Set-Location $git_root;
+try
+{
+    Set-Location $git_root;
 
-$deployment_path = Join-Path -Path $git_root -ChildPath "deployment"
-$childs = Get-ChildItem -Path $deployment_path -Directory | Out-Null
+    $deployment_path = Join-Path -Path $git_root -ChildPath "deployment"
+    $childs = Get-ChildItem -Path $deployment_path -Directory
+
+    foreach ($child in $childs)
+    {
+        if ($child.Name -eq "united-manufacturing-hub")
+        {
+            continue
+        }
+
+        $microservice_path = Join-Path -Path $deployment_path -ChildPath $child.Name
+        $docker_file_path = Join-Path -Path $microservice_path -ChildPath "Dockerfile"
 
 
-for ($i = 0; $i -lt $childs.Count; $i++) {
-    $child = $childs[$i]
-    Write-Output $child
+        if (-not((Test-Path -Path $docker_file_path -PathType Leaf)))
+        {
+            continue
+        }
+        $tag_name = "unitedmanufacturinghub/$( $child.Name ):$docker_tag"
+
+        Write-Output $child.Name
+        Write-Output $tag_name
+
+        Write-Output $docker_file_path
+        docker build -f $docker_file_path -t $tag_name .
+        docker push $tag_name
+    }
 }
-
-Set-Location $current_location;
+finally
+{
+    Set-Location $current_location
+}
