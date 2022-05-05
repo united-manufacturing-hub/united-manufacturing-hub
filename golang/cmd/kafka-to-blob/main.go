ce59f23e50775f9fd4d0db3580eea7e51f1c9912
Ferdinand Linnenberg <fl@umh.app>
2022-05-05 17:48:40 +0200

    feat: Updated all kafka services to be able to use MTLS

--- golang/cmd/kafka-to-blob/main.go
+++ golang/cmd/kafka-to-blob/main.go
@@ -48,10 +48,31 @@ func main() {
 	MinioBucketName := os.Getenv("BUCKET_NAME")
 
 	zap.S().Debugf("Setting up Kafka")
+	securityProtocol := "plaintext"
+	if internal.EnvIsTrue("KAFKA_USE_SSL") {
+		securityProtocol = "ssl"
+
+		_, err := os.Open("/SSL_certs/tls.key")
+		if err != nil {
+			panic("SSL key file not found")
+		}
+		_, err = os.Open("/SSL_certs/tls.crt")
+		if err != nil {
+			panic("SSL cert file not found")
+		}
+		_, err = os.Open("/SSL_certs/ca.crt")
+		if err != nil {
+			panic("SSL CA cert file not found")
+		}
+	}
 	internal.SetupKafka(kafka.ConfigMap{
-		"bootstrap.servers": KafkaBoostrapServer,
-		"security.protocol": "plaintext",
-		"group.id":          "kafka-to-blob",
+		"bootstrap.servers":        KafkaBoostrapServer,
+		"security.protocol":        securityProtocol,
+		"ssl.key.location":         "/SSL_certs/tls.key",
+		"ssl.key.password":         os.Getenv("KAFKA_SSL_KEY_PASSWORD"),
+		"ssl.certificate.location": "/SSL_certs/tls.crt",
+		"ssl.ca.location":          "/SSL_certs/ca.crt",
+		"group.id":                 "kafka-to-blob",
 	})
 
 	err := internal.CreateTopicIfNotExists(KafkaBaseTopic)
