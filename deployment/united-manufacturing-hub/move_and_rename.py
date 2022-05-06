dd9b4e011c18e06c570bf42d75a56dfb2958399a
Ferdinand Linnenberg <fl@umh.app>
2022-05-06 13:55:13 +0200

    fix: Corrected SAN for kafka broker

--- deployment/united-manufacturing-hub/move_and_rename.py
+++ deployment/united-manufacturing-hub/move_and_rename.py
@@ -3,6 +3,10 @@ import shutil
 
 
 def main():
+    """
+    Moves helm templates into subdirectories based on the templates name.
+    :return:
+    """
     directory = "templates"
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
@@ -10,23 +14,22 @@ def main():
             if "NOTES" in filename or ".tpl" in filename or "ServiceAccount" in filename:
                 continue
 
-            oldpath_wo_extension = os.path.splitext(filename)[0]
-            # print(filename)
+            old_file_name_without_extension = os.path.splitext(filename)[0]
 
-            disallowed = ["-deployment", "-pvc", "-pdb", "-statefulset", "-ingress", "-config-secret", "-secrets",
+            type_endings = ["-deployment", "-pvc", "-pdb", "-statefulset", "-ingress", "-config-secret", "-secrets",
                           "-persistentvolumeclaim", "-secret", "-configmap", "-local-service", "-hpa", "-service",
                           "-flows"]
 
-            newpath = oldpath_wo_extension.lower()
-            for dis in disallowed:
-                newpath = newpath.replace(dis, "")
-            newpathX = os.path.join(directory, newpath)
-            os.makedirs(newpathX, exist_ok=True)
+            new_file_name = old_file_name_without_extension.lower()
+            for dis in type_endings:
+                new_file_name = new_file_name.replace(dis, "")
+            new_path = os.path.join(directory, new_file_name)
+            os.makedirs(new_path, exist_ok=True)
 
-            newname = filename.replace(newpath, "").replace("-", "").lower()
-            print("{} -> {}\\{}".format(f, newpathX, newname))
+            newname = filename.replace(new_file_name, "").replace("-", "").lower()
+            print("{} -> {}\\{}".format(f, new_path, newname))
 
-            shutil.copy(f, os.path.join(newpathX, newname))
+            shutil.copy(f, os.path.join(new_path, newname))
 
 
 if __name__ == "__main__":
