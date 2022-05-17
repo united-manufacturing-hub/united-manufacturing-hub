1ec6f8e8a1b65258b8638cf37f20d4694e614c7c
Ferdinand Linnenberg <fl@umh.app>
2022-05-17 16:26:37 +0200

    fix: staticcheck lints

--- golang/cmd/kafka-to-postgresql/pxUniqueProduct.go
+++ golang/cmd/kafka-to-postgresql/pxUniqueProduct.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
 	jsoniter "github.com/json-iterator/go"
 	kafka2 "github.com/united-manufacturing-hub/umh-lib/v2/kafka"
@@ -50,13 +49,13 @@ func (c UniqueProduct) ProcessMessages(msg kafka2.ParsedMessage) (putback bool, 
 	}
 	AssetTableID, success := GetAssetTableID(msg.CustomerId, msg.Location, msg.AssetId)
 	if !success {
-		return true, errors.New(fmt.Sprintf("Failed to get AssetTableID for CustomerId: %s, Location: %s, AssetId: %s", msg.CustomerId, msg.Location, msg.AssetId))
+		return true, fmt.Errof("Failed to get AssetTableID for CustomerId: %s, Location: %s, AssetId: %s", msg.CustomerId, msg.Location, msg.AssetId)
 	}
 
 	var ProductTableId uint32
 	ProductTableId, success = GetProductTableId(*sC.ProductId, AssetTableID)
 	if !success {
-		return true, errors.New(fmt.Sprintf("Failed to get ProductTableId for ProductId: %s, AssetTableID: %d", *sC.ProductId, AssetTableID))
+		return true, fmt.Errof("Failed to get ProductTableId for ProductId: %s, AssetTableID: %d", *sC.ProductId, AssetTableID)
 	}
 
 	// Changes should only be necessary between this marker
