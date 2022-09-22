72b392eea65de382718ebc70e492212d14c42887
Ferdinand Linnenberg <fl@umh.app>
2022-09-22 18:46:40 +0200

    fix: added PVS to mqtt->kafka regex (#1268)

--- golang/internal/kafkaTopics.go
+++ golang/internal/kafkaTopics.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-var KafkaUMHTopicRegex = `^ia\.((raw\.(\d|-|\w|_|\.)+)|(rawImage\.(\d|-|\w|_)+\.(\d|-|\w|_)+)|(((\d|-|\w|_)+)\.((\d|-|\w|_)+)\.((\d|-|\w|_)+)\.((count|scrapCount|barcode|activity|detectedAnomaly|addShift|addOrder|addProduct|startOrder|endOrder|productImage|productTag|productTagString|addParentToChild|state|cycleTimeTrigger|uniqueProduct|scrapUniqueProduct|recommendations)|(processValue(\.(\d|-|\w|_)+)*))))$`
+var KafkaUMHTopicRegex = `^ia\.((raw\.(\d|-|\w|_|\.)+)|(rawImage\.(\d|-|\w|_)+\.(\d|-|\w|_)+)|(((\d|-|\w|_)+)\.((\d|-|\w|_)+)\.((\d|-|\w|_)+)\.((count|scrapCount|barcode|activity|detectedAnomaly|addShift|addOrder|addProduct|startOrder|endOrder|productImage|productTag|productTagString|addParentToChild|state|cycleTimeTrigger|uniqueProduct|scrapUniqueProduct|recommendations)|(processValue(\.(\d|-|\w|_)+)*)|(processValueString(\.(\d|-|\w|_)+)*))))$`
 var KafkaUMHTopicCompiledRegex = regexp.MustCompile(KafkaUMHTopicRegex)
 
 func IsKafkaTopicValid(topic string) bool {
