0f14be2838189f2adbbcb7b501b138789c43f8a4
Ferdinand Linnenberg <fl@umh.app>
2022-10-10 08:51:59 +0200

    Check regex for new datamodel (#1298)
    * feat: marked old kafka topic checker as deprecated
    
    * feat: GetTopicInformation for new datamodel
    
    * fix: fixed deprecation notice
    
    * docs: documented usage of regex
    
    * fix: modified regex to match latest revision of draft

--- golang/internal/kafkaTopics_test.go
+++ golang/internal/kafkaTopics_test.go
@@ -1,101 +1,97 @@
 package internal
 
 import (
-	"github.com/go-playground/assert/v2"
 	"testing"
 )
 
-func TestGTICount(t *testing.T) {
-	count := "ia.a.b.c.count"
-	countGTI := GetTopicInformationCached(count)
-	assert.NotEqual(t, countGTI, nil)
-	assert.Equal(t, countGTI.CustomerId, "a")
-	assert.Equal(t, countGTI.Location, "b")
-	assert.Equal(t, countGTI.AssetId, "c")
-	assert.Equal(t, countGTI.Topic, "count")
-	assert.Equal(t, len(countGTI.ExtendedTopics), 0)
-	assert.Equal(t, countGTI.TransmitterId, nil)
-	assert.Equal(t, countGTI.MacAddressOfCamera, nil)
+var validCustomExamples = []string{
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.custom.processValue.abc",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.custom.processValueString.abc",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.custom.processValue.abc",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.custom.processValue.abc.aawd_awdq2-dk-d-d-d.dd.desfi8dye_-",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.custom.processValueString.abc",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.custom.processValueString.abc.adw.adw_axxa1AA",
 }
 
-func TestGTICountLongerNames(t *testing.T) {
-	count := "ia.abc.def.gih.count"
-	countGTI := GetTopicInformationCached(count)
-	assert.NotEqual(t, countGTI, nil)
-	assert.Equal(t, countGTI.CustomerId, "abc")
-	assert.Equal(t, countGTI.Location, "def")
-	assert.Equal(t, countGTI.AssetId, "gih")
-	assert.Equal(t, countGTI.Topic, "count")
-	assert.Equal(t, len(countGTI.ExtendedTopics), 0)
-	assert.Equal(t, countGTI.TransmitterId, nil)
-	assert.Equal(t, countGTI.MacAddressOfCamera, nil)
+var validStandardExamples = []string{
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.job.add",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.job.delete",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.job.end",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.shift.add",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.shift.delete",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.product-type.add",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.product.add",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.product.modify",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.state.add",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.state.overwrite",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.state.activity",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.standard.state.reason",
 }
 
-func TestGTIRaw(t *testing.T) {
-	raw := "ia.raw.a"
-	rawGTI := GetTopicInformationCached(raw)
-	assert.NotEqual(t, rawGTI, nil)
-	assert.Equal(t, rawGTI.CustomerId, "")
-	assert.Equal(t, rawGTI.Location, "")
-	assert.Equal(t, rawGTI.AssetId, "")
-	assert.Equal(t, rawGTI.Topic, "raw")
-	assert.Equal(t, rawGTI.ExtendedTopics, []string{"a"})
-	assert.Equal(t, rawGTI.TransmitterId, nil)
-	assert.Equal(t, rawGTI.MacAddressOfCamera, nil)
+var validRawExamples = []string{
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.raw.raw",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.raw.raw",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.raw.rawImage.abc.00-B0-D0-63-C2-26",
 }
 
-func TestGTIRawImage(t *testing.T) {
-	rawImage := "ia.rawImage.a.b"
-	rawImageGTI := GetTopicInformationCached(rawImage)
-	assert.NotEqual(t, rawImageGTI, nil)
-	assert.Equal(t, rawImageGTI.CustomerId, "")
-	assert.Equal(t, rawImageGTI.Location, "")
-	assert.Equal(t, rawImageGTI.AssetId, "")
-	assert.Equal(t, rawImageGTI.Topic, "rawImage")
-	assert.Equal(t, len(rawImageGTI.ExtendedTopics), 0)
-	assert.Equal(t, *rawImageGTI.TransmitterId, "a")
-	assert.Equal(t, *rawImageGTI.MacAddressOfCamera, "b")
-}
-func TestGTIProcessValue(t *testing.T) {
-	count := "ia.a.b.c.processValue"
-	countGTI := GetTopicInformationCached(count)
-	assert.NotEqual(t, countGTI, nil)
-	assert.Equal(t, countGTI.CustomerId, "a")
-	assert.Equal(t, countGTI.Location, "b")
-	assert.Equal(t, countGTI.AssetId, "c")
-	assert.Equal(t, countGTI.Topic, "processValue")
-	assert.Equal(t, len(countGTI.ExtendedTopics), 0)
-	assert.Equal(t, countGTI.TransmitterId, nil)
-	assert.Equal(t, countGTI.MacAddressOfCamera, nil)
-}
-func TestGTIProcessValueABC(t *testing.T) {
-	count := "ia.a.b.c.processValue.a.b.c"
-	countGTI := GetTopicInformationCached(count)
-	assert.NotEqual(t, countGTI, nil)
-	assert.Equal(t, countGTI.CustomerId, "a")
-	assert.Equal(t, countGTI.Location, "b")
-	assert.Equal(t, countGTI.AssetId, "c")
-	assert.Equal(t, countGTI.Topic, "processValue")
-	assert.Equal(t, len(countGTI.ExtendedTopics), 3)
-	assert.Equal(t, countGTI.TransmitterId, nil)
-	assert.Equal(t, countGTI.MacAddressOfCamera, nil)
+var invalidExamples = []string{
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.custom",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.custom.",
+	"umh.v1.exampleEnterprise.exampleSite.exampleArea.exampleProductionLine.exampleWorkCell.abc.aa",
 }
 
-func BenchmarkRegex(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		getTopicInformation("ia.a.b.c.count")
-		getTopicInformation("ia.raw.a")
-		getTopicInformation("ia.rawImage.a.b")
-		getTopicInformation("ia.a.b.c.INVALID")
+func TestIsKafkaTopicV1Valid(t *testing.T) {
+
+	for _, example := range validCustomExamples {
+		if !IsKafkaTopicV1Valid(example) {
+			t.Errorf("Topic %s should be valid", example)
+		}
+	}
+	for _, example := range validStandardExamples {
+		if !IsKafkaTopicV1Valid(example) {
+			t.Errorf("Topic %s should be valid", example)
+		}
+	}
+	for _, example := range validRawExamples {
+		if !IsKafkaTopicV1Valid(example) {
+			t.Errorf("Topic %s should be valid", example)
+		}
+	}
+
+	for _, example := range invalidExamples {
+		if IsKafkaTopicV1Valid(example) {
+			t.Errorf("Topic %s should be invalid", example)
+		}
 	}
 }
 
-func BenchmarkCachedRegex(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		GetTopicInformationCached("ia.a.b.c.count")
-		GetTopicInformationCached("ia.raw.a")
-		GetTopicInformationCached("ia.rawImage.a.b")
-		GetTopicInformationCached("ia.a.b.c.INVALID")
+func TestGetTopicInformationV1(t *testing.T) {
+	for _, example := range validCustomExamples {
+		ti, err := getTopicInformationV1(example)
+		if err != nil {
+			t.Errorf("Topic %s should be valid", example)
+		}
+		if ti.Display() != example {
+			t.Errorf("Topic:\n\t%s\nshould have display\n\t%s\n", ti.Display(), example)
+		}
+	}
+	for _, example := range validStandardExamples {
+		ti, err := getTopicInformationV1(example)
+		if err != nil {
+			t.Errorf("Topic %s should be valid", example)
+		}
+		if ti.Display() != example {
+			t.Errorf("Topic:\n\t%s\nshould have display\n\t%s\n", ti.Display(), example)
+		}
 	}
 
+	for _, example := range validRawExamples {
+		ti, err := getTopicInformationV1(example)
+		if err != nil {
+			t.Errorf("Topic %s should be valid", example)
+		}
+		if ti.Display() != example {
+			t.Errorf("Topic:\n\t%s\nshould have display\n\t%s\n", ti.Display(), example)
+		}
+	}
 }
