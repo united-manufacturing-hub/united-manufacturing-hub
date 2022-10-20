2c2aabb9914d9657721af7565877433d8ffd2093
Ferdinand Linnenberg <fl@umh.app>
2022-10-20 14:53:13 +0200

    fix: typos

--- deployment/grafana-plugin-extractor/runner.sh
+++ deployment/grafana-plugin-extractor/runner.sh
@@ -4,8 +4,8 @@ mkdir -p /var/lib/grafana/plugins
 
 # Delete old versions if exist
 rm -rf /var/lib/grafana/plugins/umh-datasource
-rf -rf /var/lib/grafana/plugins/umh-factoryinput-panel
-rf -rf /var/lib/grafana/plugins/umh-v2-datasource
+rm -rf /var/lib/grafana/plugins/umh-factoryinput-panel
+rm -rf /var/lib/grafana/plugins/umh-v2-datasource
 
 cp -r /umh-datasource /var/lib/grafana/plugins/umh-datasource
 cp -r /umh-factoryinput-panel /var/lib/grafana/plugins/umh-factoryinput-panel
@@ -16,9 +16,12 @@ ls -la /var/lib/grafana/plugins
 
 echo "UMH-datasource:"
 ls -la /var/lib/grafana/plugins/umh-datasource
+cat /var/lib/grafana/plugins/umh-datasource/plugin.json
 
 echo "UMH-factoryinput-panel:"
 ls -la /var/lib/grafana/plugins/umh-factoryinput-panel
+cat /var/lib/grafana/plugins/umh-factoryinput-panel/plugin.json
 
 echo "UMH-v2-datasource:"
 ls -la /var/lib/grafana/plugins/umh-v2-datasource
+cat /var/lib/grafana/plugins/umh-v2-datasource/plugin.json
