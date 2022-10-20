5f31b89aac552afb1c9c70bdb3b9b931ab8ee305
Ferdinand Linnenberg <fl@umh.app>
2022-10-20 14:19:30 +0200

    fix: delete old versions before updating

--- deployment/grafana-plugin-extractor/runner.sh
+++ deployment/grafana-plugin-extractor/runner.sh
@@ -1,15 +1,24 @@
 #!/bin/env ash
 
 mkdir -p /var/lib/grafana/plugins
+
+# Delete old versions if exist
+rm -rf /var/lib/grafana/plugins/umh-datasource
+rf -rf /var/lib/grafana/plugins/umh-factoryinput-panel
+rf -rf /var/lib/grafana/plugins/umh-datasource-v2
+
 cp -r /umh-datasource /var/lib/grafana/plugins/umh-datasource
 cp -r /umh-factoryinput-panel /var/lib/grafana/plugins/umh-factoryinput-panel
 cp -r /umh-datasource-v2 /var/lib/grafana/plugins/umh-datasource-v2
 
 echo "Grafana plugins extracted"
 ls -la /var/lib/grafana/plugins
+
 echo "UMH-datasource:"
 ls -la /var/lib/grafana/plugins/umh-datasource
+
 echo "UMH-factoryinput-panel:"
 ls -la /var/lib/grafana/plugins/umh-factoryinput-panel
+
 echo "UMH-datasource-v2:"
 ls -la /var/lib/grafana/plugins/umh-datasource-v2
