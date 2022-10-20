d9df44d8069b4f29622633d496e897033c67f365
Ferdinand Linnenberg <fl@umh.app>
2022-10-20 14:42:31 +0200

    fix: umh-datasource-v2 -> umh-v2-datasource

--- deployment/grafana-plugin-extractor/runner.sh
+++ deployment/grafana-plugin-extractor/runner.sh
@@ -5,11 +5,11 @@ mkdir -p /var/lib/grafana/plugins
 # Delete old versions if exist
 rm -rf /var/lib/grafana/plugins/umh-datasource
 rf -rf /var/lib/grafana/plugins/umh-factoryinput-panel
-rf -rf /var/lib/grafana/plugins/umh-datasource-v2
+rf -rf /var/lib/grafana/plugins/umh-v2-datasource
 
 cp -r /umh-datasource /var/lib/grafana/plugins/umh-datasource
 cp -r /umh-factoryinput-panel /var/lib/grafana/plugins/umh-factoryinput-panel
-cp -r /umh-datasource-v2 /var/lib/grafana/plugins/umh-datasource-v2
+cp -r /umh-v2-datasource /var/lib/grafana/plugins/umh-v2-datasource
 
 echo "Grafana plugins extracted"
 ls -la /var/lib/grafana/plugins
@@ -20,5 +20,5 @@ ls -la /var/lib/grafana/plugins/umh-datasource
 echo "UMH-factoryinput-panel:"
 ls -la /var/lib/grafana/plugins/umh-factoryinput-panel
 
-echo "UMH-datasource-v2:"
-ls -la /var/lib/grafana/plugins/umh-datasource-v2
+echo "UMH-v2-datasource:"
+ls -la /var/lib/grafana/plugins/umh-v2-datasource
