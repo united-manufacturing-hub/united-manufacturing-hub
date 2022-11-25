5d720ffa5ba91d8c205c1529c9fc1e384cfb546d
Ferdinand Linnenberg <fl@umh.app>
2022-11-25 16:45:19 +0100

    feat: prometheus and heartbeat

--- deployment/hivemq-init/runner.sh
+++ deployment/hivemq-init/runner.sh
@@ -2,6 +2,9 @@
 
 echo "Copying hivemq-file-rbac-extension to extensions directory"
 cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-file-rbac-extension
+cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-prometheus-extension
+cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-heartbeat-extension
+
 
 echo "Listing extensions directory"
 ls -la /opt/hivemq-ce-2022.1/extensions
