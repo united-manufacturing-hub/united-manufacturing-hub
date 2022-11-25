0ae7db4ab5d75f55968094828dae6afd03905abb
Ferdinand Linnenberg <fl@umh.app>
2022-11-25 14:08:33 +0100

    feat: cleaner.sh no longer needed

--- deployment/hivemq-init/runner.sh
+++ deployment/hivemq-init/runner.sh
@@ -3,8 +3,5 @@
 echo "Copying hivemq-file-rbac-extension to extensions directory"
 cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-file-rbac-extension
 
-echo "Copying cleaner script to extensions directory"
-cp /cleaner.sh /opt/hivemq-ce-2022.1/extensions/cleaner.sh
-
 echo "Listing extensions directory"
 ls -la /opt/hivemq-ce-2022.1/extensions
