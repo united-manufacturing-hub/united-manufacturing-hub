1b1c46fdeeeae7fdca501f0d9f00ae467fa8c1c6
Ferdinand Linnenberg <fl@umh.app>
2022-11-25 17:49:03 +0100

    fix: incorrect docs

--- deployment/hivemq-init/runner.sh
+++ deployment/hivemq-init/runner.sh
@@ -2,8 +2,8 @@
 
 echo "Copying hivemq-file-rbac-extension to extensions directory"
 cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-file-rbac-extension
-cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-prometheus-extension
-cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-heartbeat-extension
+cp -r /hivemq-prometheus-extension /opt/hivemq-ce-2022.1/extensions/hivemq-prometheus-extension
+cp -r /hivemq-heartbeat-extension /opt/hivemq-ce-2022.1/extensions/hivemq-heartbeat-extension
 
 
 echo "Listing extensions directory"
