ce18d7f7c1120de18b25188e9cef81fb6d681226
Ferdinand Linnenberg <fl@umh.app>
2022-11-25 13:58:20 +0100

    fix: added output to init script

--- deployment/hivemq-init/runner.sh
+++ deployment/hivemq-init/runner.sh
@@ -1,6 +1,10 @@
 #!/bin/env ash
 
-mkdir -p /opt/hivemq-ce-2022.1/extensions
-
+echo "Copying hivemq-file-rbac-extension to extensions directory"
 cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-file-rbac-extension
+
+echo "Copying cleaner script to extensions directory"
 cp /cleaner.sh /opt/hivemq-ce-2022.1/extensions/cleaner.sh
+
+echo "Listing extensions directory"
+ls -la /opt/hivemq-ce-2022.1/extensions
