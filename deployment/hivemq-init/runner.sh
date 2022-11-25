9cfece7e4ddadb6ed0ac7e688156ab065a5bb471
Ferdinand Linnenberg <fl@umh.app>
2022-11-25 13:18:47 +0100

    fix: add sidecar to remove allow-all extensions

--- deployment/hivemq-init/runner.sh
+++ deployment/hivemq-init/runner.sh
@@ -1,7 +1,5 @@
 #!/bin/env ash
 
-# Delete hivemq-allow-all-extensions
-rm -rf /opt/hivemq-ce-2022.1/extensions/hivemq-allow-all-extensions
-
+mkdir -p /opt/hivemq-ce-2022.1/extensions
 
 cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-file-rbac-extension
