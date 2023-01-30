7ab21203db054c39cb78491773c92401f24ae501
Matteo Danelon <md@umh.app>
2023-01-30 10:46:02 +0100

    feat: make hivemq rbac optional

--- deployment/hivemq-init/runner.sh
+++ deployment/hivemq-init/runner.sh
@@ -6,6 +6,7 @@ if [ "$RBAC_ENABLED" = "true" ]; then
     cp -r /hivemq-file-rbac-extension /opt/hivemq-ce-2022.1/extensions/hivemq-file-rbac-extension
 else
     echo "RBAC is disabled. Skipping hivemq-file-rbac-extension"
+    cp -r /hivemq-allow-all-extension /opt/hivemq-ce-2022.1/extensions/hivemq-allow-all-extension
 fi
 cp -r /hivemq-prometheus-extension /opt/hivemq-ce-2022.1/extensions/hivemq-prometheus-extension
 cp -r /hivemq-heartbeat-extension /opt/hivemq-ce-2022.1/extensions/hivemq-heartbeat-extension
