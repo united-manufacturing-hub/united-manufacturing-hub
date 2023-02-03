cbece3998f40f57f9d0396c023364c8c4d79b43e
Matteo Danelon <md@umh.app>
2023-02-03 16:22:40 +0100

    ci: failing test pipeline TO BE REVERTED

--- test/data-flow-test-script.sh
+++ test/data-flow-test-script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-TIMESTAMP_TO=$(date +%FT%TZ)
-TIMESTAMP_FROM=$(date +%FT%TZ --date='5 min ago')
+TIMESTAMP_TO=$(date +%FT%TZ --date='1 day ago')
+TIMESTAMP_FROM=$(date +%FT%TZ --date='5 day ago')
 
 AUTH_HEADER="Authorization: ${FACTORYINSIGHT_APIKEY}"
 URL="${FACTORYINSIGHT_BASEURL}/api/v2/factoryinsight/testLocation/DefaultArea/DefaultProductionLine/testMachine/tags/custom/Pressure?from=${TIMESTAMP_FROM}&to=${TIMESTAMP_TO}&gapFilling=null&tagAggregates=avg&timeBucket=1m&includePrevious=true&includeNext=true&includeRunning=true&keepStatesInteger=true'"
