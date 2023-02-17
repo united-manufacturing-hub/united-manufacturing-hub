f9c5a5679da7b6fa2b7a313d38c8a8bd5f4a6860
Matteo Danelon <md@umh.app>
2023-02-17 16:55:40 +0100

    ci: better log output

--- tools/save_logs.sh
+++ tools/save_logs.sh
@@ -17,35 +17,35 @@ jobs=$(kubectl get jobs -n united-manufacturing-hub -o name)
 # Get logs for all deployments
 for deployment in $deployments; do
     log="${deployment//*united-manufacturing-hub-}"
-    kubectl logs "$deployment" -n united-manufacturing-hub > "$log".log || true
+    kubectl logs "$deployment" -n united-manufacturing-hub --all-containers --prefix > "$log".log || true
 done
 
 # Get logs for all statefulsets
 for statefulset in $statefulsets; do
     log="${statefulset//*united-manufacturing-hub-}"
-    kubectl logs "$statefulset" -n united-manufacturing-hub > "$log".log || true
+    kubectl logs "$statefulset" -n united-manufacturing-hub --all-containers --prefix > "$log".log || true
 done
 
 # Get logs for all jobs
 for job in $jobs; do
     log="${job//*united-manufacturing-hub-}"
-    kubectl logs "$job" -n united-manufacturing-hub > "$log".log || true
+    kubectl logs "$job" -n united-manufacturing-hub --all-containers --prefix > "$log".log || true
 done
 
 # Get all pods and services
-kubectl get po,svc -n united-manufacturing-hub > pods_and_services.log
+kubectl get po,svc -n united-manufacturing-hub > kubectl_pods_and_services.log
 
 # Get all events
-kubectl get events -n united-manufacturing-hub > events.log
+kubectl get events -n united-manufacturing-hub > kubectl_events.log
 
 # Describe all pods
-kubectl describe po -n united-manufacturing-hub > describe_pods.log
+kubectl describe po -n united-manufacturing-hub > kubectl_describe_pods.log
 
 # Describe all services
-kubectl describe svc -n united-manufacturing-hub > describe_services.log
+kubectl describe svc -n united-manufacturing-hub > kubectl_describe_services.log
 
 # Helm status
-helm status united-manufacturing-hub -n united-manufacturing-hub > helm_status.log
+helm status united-manufacturing-hub -n united-manufacturing-hub > kubectl_helm_status.log
 
 # cd back to original directory
 cd - || exit
