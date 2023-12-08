fb28d7540d0f7bbbf9e4f18e63b4d07bf376a70e
Ferdinand Linnenberg <fl@umh.app>
2023-12-08 09:28:09 +0100

    feat: Updated sarama kafka wrapper, allowing topic changes during runtime (#1705)
    * feat: updated wrapper
    
    * fix: updated sarama wrapper
    
    * fix: use race runner
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: set message channel to read only
    
    * fix: removed close
    
    * fix: updated sarama wrapper
    
    * fix: added go work sum
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: updated sarama wrapper
    
    * fix: commit work.sum
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- golang/vendor/github.com/united-manufacturing-hub/Sarama-Kafka-Wrapper-2/pkg/kafka/consumer/redpanda/client.go
+++ golang/vendor/github.com/united-manufacturing-hub/Sarama-Kafka-Wrapper-2/pkg/kafka/consumer/redpanda/client.go
@@ -2,347 +2,349 @@ package redpanda
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
 	"github.com/IBM/sarama"
 	"github.com/united-manufacturing-hub/Sarama-Kafka-Wrapper-2/pkg/kafka/shared"
 	"go.uber.org/zap"
-	"net/http"
 	"regexp"
-	"strings"
+	"slices"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
 )
 
+// Consumer represents a Kafka consumer.
 type Consumer struct {
-	rawClient               sarama.Client
-	httpClients             []string
-	consumerGroup           *sarama.ConsumerGroup
-	regexTopics             []regexp.Regexp
-	actualTopics            []string
-	actualTopicsLock        sync.RWMutex
-	running                 atomic.Bool
-	cgContext               context.Context
-	cgCncl                  context.CancelFunc
-	incomingMessages        chan *shared.KafkaMessage
-	messagesToMark          chan *shared.KafkaMessage
-	markedMessages          atomic.Uint64
-	consumedMessages        atomic.Uint64
-	consumerShutdownChannel chan bool
-	groupName               string
-	greeter                 bool
+	// subscribeRegexes holds compiled regular expressions to filter topics.
+	subscribeRegexes []*regexp.Regexp
+
+	// topics holds the list of Kafka topics the consumer is subscribed to.
+	topics []string
+
+	// topicsMutex provides concurrent access protection for the topics.
+	topicsMutex sync.RWMutex
+
+	// groupId represents the Kafka consumer group ID.
+	groupId string
+
+	// incomingMessages is a channel for incoming Kafka messages.
+	incomingMessages chan *shared.KafkaMessage
+
+	// messagesToMarkChan is a channel for marking messages as processed.
+	messagesToMarkChan chan *shared.KafkaMessage
+
+	// read tracks the number of messages read.
+	read atomic.Uint64
+
+	// marked tracks the number of messages marked as processed.
+	marked atomic.Uint64
+
+	// isReady indicates if the consumer is ready to consume messages.
+	isReady atomic.Bool
+
+	// config holds the Sarama consumer configuration.
+	config *sarama.Config
+
+	// brokers lists the Kafka brokers.
+	brokers []string
+
+	// client is the Sarama client for Kafka communication.
+	client *sarama.Client
+
+	// consumerGroup is the Sarama consumer group.
+	consumerGroup *sarama.ConsumerGroup
 }
 
-// GetStats returns marked and consumed message counts.
-func (c *Consumer) GetStats() (uint64, uint64) {
-	return c.markedMessages.Load(), c.consumedMessages.Load()
+// genIID generates an instance ID by appending a timestamp to the provided instanceId and hashing it.
+func genIID(instanceId string) string {
+	// Append random suffix to avoid conflicts
+	// For our purposes, unix nanoseconds is good enough
+	now := time.Now().UnixNano()
+	x := instanceId + "-" + strconv.FormatInt(now, 10)
+	// Hash the string to avoid weird characters (using SHA-256)
+	hasher := sha256.New()
+	hasher.Write([]byte(x))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-func NewConsumer(kafkaBrokers, httpBrokers, subscribeRegexes []string, groupName, instanceId string, greeter bool) (*Consumer, error) {
-	zap.S().Infof("connecting to brokers: %v", kafkaBrokers)
+// NewConsumer initializes and returns a new Consumer instance.
+func NewConsumer(kafkaBrokers, subscribeRegexes []string, groupId, instanceId string) (*Consumer, error) {
+	zap.S().Infof("Connecting to brokers: %v", kafkaBrokers)
+	zap.S().Infof("Creating new consumer with Group ID: %s, Instance ID: %s", groupId, instanceId)
+	zap.S().Infof("Subscribing to topics: %v", subscribeRegexes)
+
+	sarama.Logger = zap.NewStdLog(zap.L())
+
 	config := sarama.NewConfig()
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 	config.Consumer.Offsets.AutoCommit.Enable = true
 	config.Consumer.Offsets.AutoCommit.Interval = 1 * time.Second
-	config.Consumer.Group.InstanceId = instanceId
+	config.Consumer.Group.InstanceId = genIID(instanceId)
 	config.Version = sarama.V2_3_0_0
+	config.Metadata.RefreshFrequency = 1 * time.Minute
 
-	c, err := sarama.NewClient(kafkaBrokers, config)
-	if err != nil {
-		return nil, err
-	}
-	zap.S().Infof("connected to brokers: %v", kafkaBrokers)
-
-	var rgxTopics []regexp.Regexp
-	for _, t := range subscribeRegexes {
-		rgx, err := regexp.Compile(t)
+	c := Consumer{}
+	c.subscribeRegexes = make([]*regexp.Regexp, len(subscribeRegexes))
+	for i, regex := range subscribeRegexes {
+		re, err := regexp.Compile(regex)
 		if err != nil {
+			zap.S().Errorf("Failed to compile regex: %v", err)
 			return nil, err
 		}
-		rgxTopics = append(rgxTopics, *rgx)
+		c.subscribeRegexes[i] = re
 	}
-	return &Consumer{
-		rawClient:               c,
-		httpClients:             httpBrokers,
-		regexTopics:             rgxTopics,
-		actualTopics:            []string{},
-		running:                 atomic.Bool{},
-		groupName:               groupName,
-		markedMessages:          atomic.Uint64{},
-		consumerShutdownChannel: make(chan bool, 512), // This is oversized, but it's better to be safe than sorry
-		incomingMessages:        make(chan *shared.KafkaMessage, 100_000),
-		messagesToMark:          make(chan *shared.KafkaMessage, 100_000),
-		greeter:                 greeter,
-	}, nil
-}
+	c.groupId = groupId
+	c.config = config
+	c.brokers = kafkaBrokers
 
-func (c *Consumer) GetTopics() []string {
-	return c.actualTopics
-}
+	zap.S().Debugf("Setting up channels")
+	c.incomingMessages = make(chan *shared.KafkaMessage, 100_000)
+	c.messagesToMarkChan = make(chan *shared.KafkaMessage, 100_000)
 
-func (c *Consumer) Start() error {
-	if c.running.Swap(true) {
-		return nil
+	zap.S().Debugf("Setting up initial client")
+	newClient, err := sarama.NewClient(kafkaBrokers, config)
+	if err != nil {
+		zap.S().Errorf("Failed to create new client: %v", err)
+		return nil, err
 	}
-	c.cgContext, c.cgCncl = context.WithCancel(context.Background())
 
-	go c.generateTopics()
-	go c.consumer()
-	go c.reporter()
-	return nil
-}
-
-func (c *Consumer) generateTopics() {
-	zap.S().Debugf("Started topic generator")
-
-	var httpClient http.Client
-	httpClient.Timeout = 5 * time.Second
-
-	ticker := time.NewTicker(5 * time.Second)
-
-	for c.running.Load() {
-		<-ticker.C
-
-		clients := c.httpClients
-		topics := make(map[string]bool)
-		for _, client := range clients {
-			url := fmt.Sprintf("http://%s/topics", client)
-			zap.S().Infof("fetching topics from %s", url)
-			response, err := httpClient.Get(url)
-			if err != nil {
-				zap.S().Errorf("failed to fetch topics from %s: %v", url, err)
-				continue
-			}
-			zap.S().Debugf("Finished http request")
-
-			// Parse as []string from JSON
-			var topicsX []string
-			err = json.NewDecoder(response.Body).Decode(&topicsX)
-			if err != nil {
-				zap.S().Errorf("failed to parse topics from %s: %v", url, err)
-				continue
-			}
-			err = response.Body.Close()
-			if err != nil {
-				zap.S().Errorf("failed to close response body from %s: %v", url, err)
-				continue
-			}
-
-			// Add topics to map
-			for _, topic := range topicsX {
-				topics[topic] = true
-			}
-			zap.S().Debugf("Fetched %d topics from remote", len(topics))
+	for {
+		err = newClient.RefreshMetadata()
+		if err != nil {
+			zap.S().Errorf("Failed to refresh metadata: %v", err)
+			return nil, err
 		}
 
-		// Filter topics by regex
-		var actualTopics []string
-		for topic := range topics {
-			for _, rgx := range c.regexTopics {
-				if rgx.MatchString(topic) {
-					actualTopics = append(actualTopics, topic)
-					break
+		var topics []string
+		topics, err = newClient.Topics()
+		if err != nil {
+			zap.S().Errorf("Failed to retrieve topics: %v", err)
+			return nil, err
+		}
+		zap.S().Debugf("Filtering topics")
+		topics = filter(topics, c.subscribeRegexes)
+		if len(topics) > 0 {
+			c.topicsMutex.Lock()
+			c.topics = topics
+			c.topicsMutex.Unlock()
+			break
+		}
+		zap.S().Infof("No topics found. Waiting for 1 second")
+		time.Sleep(1 * time.Second)
+	}
+	err = newClient.Close()
+	if err != nil {
+		zap.S().Warnf("Failed to close initial client: %s", err)
+	}
+
+	go c.start()
+	go c.refreshTopics()
+
+	zap.S().Debugf("Consumer initialized with Group ID: %s, Instance ID: %s, Brokers: %v", groupId, instanceId, kafkaBrokers)
+	return &c, nil
+}
+
+// start begins the consumption process for Kafka messages.
+// If required it also re-initializes the consumer.
+func (c *Consumer) start() {
+	zap.S().Debugf("Starting consumer with Group ID: %s", c.groupId)
+	var err error
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	for {
+		var topics []string
+		c.topicsMutex.RLock()
+		topics = make([]string, len(c.topics))
+		copy(topics, c.topics)
+		c.topicsMutex.RUnlock()
+		if len(topics) == 0 {
+			zap.S().Infof("No topics found. Waiting for 1 second")
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		zap.S().Debugf("Got topics: %v", topics)
+
+		if c.client != nil {
+			if !(*c.client).Closed() {
+				zap.S().Infof("Closing old client")
+				err = (*c.client).Close()
+				if err != nil {
+					zap.S().Warnf("Failed to close client: %s", err)
 				}
 			}
 		}
-		zap.S().Debugf("After regex check we have %d topics", len(actualTopics))
-
-		// Check if topics changed
-		c.actualTopicsLock.RLock()
-		changed := false
-		for _, topic := range actualTopics {
-			found := false
-			for _, topic2 := range c.actualTopics {
-				if topic == topic2 {
-					found = true
-					break
-				}
-			}
-			if !found {
-				changed = true
-				if c.greeter {
-					c.incomingMessages <- &shared.KafkaMessage{
-						Topic: topic,
-						Value: []byte(""),
-					}
-				}
-				break
-			}
-		}
-		c.actualTopicsLock.RUnlock()
-
-		if changed {
-			zap.S().Infof("topics changed: %v", actualTopics)
-			c.actualTopicsLock.Lock()
-			c.actualTopics = actualTopics
-			c.actualTopicsLock.Unlock()
-			zap.S().Debugf("updated actual topics")
-			c.cgCncl()
-			shutdown(c.consumerShutdownChannel)
-			c.cgContext, c.cgCncl = context.WithCancel(context.Background())
-		} else {
-			zap.S().Debugf("topics unchanged")
-		}
-		zap.S().Debugf("Finished topic generator")
-	}
-	zap.S().Debugf("Goodbye topic generator")
-}
-
-func (c *Consumer) consumer() {
-	zap.S().Debugf("Started consumer")
-	ticker := time.NewTicker(shared.CycleTime)
-	for c.running.Load() {
-		<-ticker.C
-		zap.S().Debugf("Getting topics")
-		c.actualTopicsLock.RLock()
-		topicClone := make([]string, len(c.actualTopics))
-		copy(topicClone, c.actualTopics)
-		c.actualTopicsLock.RUnlock()
-
-		if len(topicClone) == 0 {
-			zap.S().Debugf("No topics for consume, trying later")
-			time.Sleep(5 * time.Second)
+		zap.S().Debugf("Creating new client")
+		var client sarama.Client
+		c.config.Consumer.Group.InstanceId = genIID(c.config.Consumer.Group.InstanceId)
+		zap.S().Debugf("Using instanceId %s", c.config.Consumer.Group.InstanceId)
+		client, err = sarama.NewClient(c.brokers, c.config)
+		c.client = &client
+		if err != nil {
+			zap.S().Errorf("Failed to create new client: %v", err)
+			time.Sleep(1 * time.Second)
 			continue
 		}
 
-		zap.S().Debugf("Create handler")
-		c.consumerShutdownChannel = make(chan bool, 512) // This is oversized, but it's better to be safe than sorry
-		handler := &GroupHandler{
-			incomingMessages: c.incomingMessages,
-			messagesToMark:   c.messagesToMark,
-			markedMessages:   &c.markedMessages,
-			consumedMessages: &c.consumedMessages,
-			shutdownChannel:  c.consumerShutdownChannel,
+		zap.S().Debugf("Creating new consumer")
+		consumer, err := sarama.NewConsumerGroupFromClient(c.groupId, client)
+		if err != nil {
+			zap.S().Errorf("Failed to create new consumer: %v", err)
+			time.Sleep(1 * time.Second)
+			continue
 		}
-		zap.S().Debugf("Create consumer group")
-		c.createConsumerGroup()
+		c.consumerGroup = &consumer
 
-		zap.S().Debugf("Beginning consume loop")
-		if err := (*c.consumerGroup).Consume(c.cgContext, topicClone, handler); err != nil {
-			// Check if the error is "no topics provided"
-			if err.Error() == "no topics provided" {
-				zap.S().Info("no topics provided")
-			} else if strings.Contains(err.Error(), "i/o timeout") {
-				zap.S().Info("i/o timeout, trying later")
-			} else if strings.Contains(err.Error(), "context canceled") {
-				zap.S().Info("context canceled, trying later")
-			} else if strings.Contains(err.Error(), "EOF") {
-				zap.S().Info("EOF, trying later")
-			} else {
-				zap.S().Errorf("failed to consume: %v", err)
+		// Consume loop
+		zap.S().Infof("Starting to consume messages")
+		for {
+			cgh := ConsumerGroupHandler{
+				incomingMessages:   c.incomingMessages,
+				messagesToMarkChan: c.messagesToMarkChan,
+				ready:              &c.isReady,
+				read:               &c.read,
+				marked:             &c.marked,
+			}
+			zap.S().Debugf("CHG topics: %v", topics)
+			err = consumer.Consume(ctx, topics, &cgh)
+			if errors.Is(err, sarama.ErrClosedClient) {
+				zap.S().Infof("Consumer closed")
+				time.Sleep(5 * time.Second)
+				break
+			} else if errors.Is(err, sarama.ErrClosedConsumerGroup) {
+				zap.S().Infof("Consumer group closed")
+				time.Sleep(5 * time.Second)
+				break
+			} else if err != nil {
+				zap.S().Errorf("Consumer error: %v", err)
+				time.Sleep(1 * time.Second)
+			}
+			if ctx.Err() != nil {
+				zap.S().Infof("Context closed")
+				break
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
+		zap.S().Debugf("Consumer start loop ended for Group ID: %s", c.groupId)
+	}
+}
+
+// refreshTopics periodically updates the list of topics the consumer subscribes to.
+func (c *Consumer) refreshTopics() {
+
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		zap.S().Debugf("Starting topic refresh for consumer with Group ID: %s", c.groupId)
+		<-ticker.C
+		if c.client == nil {
+			zap.S().Debugf("Client not ready")
+			continue
+		}
+		zap.S().Debugf("Refreshing metadata")
+
+		err := (*c.client).RefreshMetadata()
+		if err != nil {
+			zap.S().Errorf("Error refreshing metadata: %v", err)
+			continue
+		}
+
+		topics, err := (*c.client).Topics()
+		if err != nil {
+			zap.S().Errorf("Error getting topics: %v", err)
+			continue
+		}
+
+		topics = filter(topics, c.subscribeRegexes)
+		c.topicsMutex.RLock()
+		compare := slices.Compare(c.topics, topics)
+		c.topicsMutex.RUnlock()
+		if compare == 0 {
+			zap.S().Infof("No change in topics")
+			continue
+		}
+		c.topicsMutex.Lock()
+		zap.S().Infof("Detected topic change. Old topics: %v, New topics: %v", c.topics, topics)
+		c.topics = topics
+		c.topicsMutex.Unlock()
+
+		if c.consumerGroup != nil {
+			err = (*c.consumerGroup).Close()
+			if err != nil {
+				zap.S().Warnf("Failed to close consumer group: %s", err)
 			}
 		}
-		zap.S().Debugf("End consume loop")
+		if c.client != nil {
+			err = (*c.client).Close()
+			if err != nil {
+				zap.S().Warnf("Failed to close client: %s", err)
+			}
+		}
+		zap.S().Debugf("Refresh loop ended")
+		// Reset the ticker to avoid a burst of refreshes
+		ticker.Reset(5 * time.Second)
+		zap.S().Debugf("Topic refresh loop ended for Group ID: %s", c.groupId)
 	}
-	zap.S().Debugf("Goodbye consumer")
 }
 
-// Close terminates the Consumer.
-func (c *Consumer) Close() error {
-	zap.S().Info("closing consumer")
-	if !c.running.Swap(false) {
-		zap.S().Info("consumer already closed")
-		return nil
-	}
-	closeTimeout := 5 * time.Second
-	select {
-	case <-time.After(closeTimeout):
-		zap.S().Warnf("failed to close consumer within %s", closeTimeout)
-	case err := <-func() chan error {
-		c.cgCncl()
-		err := (*c.consumerGroup).Close()
-		chanX := make(chan error, 1)
-		chanX <- err
-		return chanX
-	}():
-		return err
-	}
-
-	return nil
+// GetStats returns consumed message counts.
+func (c *Consumer) GetStats() (uint64, uint64) {
+	return c.marked.Load(), c.read.Load()
 }
 
-// IsRunning returns the run state.
-func (c *Consumer) IsRunning() bool {
-	return c.running.Load()
+// GetTopics returns the topics that the consumer is subscribed to.
+func (c *Consumer) GetTopics() []string {
+	c.topicsMutex.RLock()
+	topics := make([]string, len(c.topics))
+	copy(topics, c.topics)
+	c.topicsMutex.RUnlock()
+	return topics
 }
 
-// GetMessage receives a single message.
+// GetMessage returns the next message from the consumer.
 func (c *Consumer) GetMessage() *shared.KafkaMessage {
-	select {
-	case msg := <-c.incomingMessages:
-		return msg
-	default:
-		return nil
-	}
+	return <-c.incomingMessages
 }
 
-// GetMessages returns the message channel.
-func (c *Consumer) GetMessages() chan *shared.KafkaMessage {
+// GetMessages returns the channel of messages from the consumer.
+func (c *Consumer) GetMessages() <-chan *shared.KafkaMessage {
 	return c.incomingMessages
 }
 
-// MarkMessage marks a message for commit.
-func (c *Consumer) MarkMessage(msg *shared.KafkaMessage) {
-	c.messagesToMark <- msg
+// MarkMessage marks a message as processed.
+func (c *Consumer) MarkMessage(message *shared.KafkaMessage) {
+	c.messagesToMarkChan <- message
 }
 
-// MarkMessages marks multiple messages for commit.
-func (c *Consumer) MarkMessages(msgs []*shared.KafkaMessage) {
-	for _, msg := range msgs {
-		c.messagesToMark <- msg
+// MarkMessages marks a slice of messages as processed.
+func (c *Consumer) MarkMessages(messages []*shared.KafkaMessage) {
+	for _, message := range messages {
+		c.messagesToMarkChan <- message
 	}
 }
 
-func (c *Consumer) createConsumerGroup() {
-	var err error
-	if c.consumerGroup != nil {
-		zap.S().Debugf("Closing existing consumer group")
-		err = (*c.consumerGroup).Close()
-		if err != nil {
-			zap.S().Errorf("Failed to close existing consumer group: %s", err)
-		}
-	}
-	zap.S().Debugf("Refreshing metadata")
-	err = c.rawClient.RefreshMetadata()
-	if err != nil {
-		zap.S().Errorf("Failed to refresh metadata: %s", err)
-	}
-	zap.S().Debugf("Creating consumer group: %v", c.groupName)
-	var cg sarama.ConsumerGroup
-	cg, err = sarama.NewConsumerGroupFromClient(c.groupName, c.rawClient)
-	if err != nil {
-		zap.S().Fatalf("Failed to create consumer group: %v", err)
-	}
-	zap.S().Debugf("Created consumer group")
-	c.consumerGroup = &cg
-
+// IsReady returns whether the consumer is ready to consume messages.
+func (c *Consumer) IsReady() bool {
+	return c.isReady.Load()
 }
 
-func (c *Consumer) reporter() {
-	ticker10Seconds := time.NewTicker(10 * time.Second)
-	for c.running.Load() {
-		<-ticker10Seconds.C
-		marked, consumed := c.GetStats()
-		zap.S().Infof("marked: %d, consumed: %d", marked, consumed)
-		zap.S().Infof("Incoming messages channel (%d/%d), Marked messages channel (%d/%d)",
-			len(c.incomingMessages), cap(c.incomingMessages),
-			len(c.messagesToMark), cap(c.messagesToMark))
-
-		if c.consumerGroup != nil {
-			for err := range (*c.consumerGroup).Errors() {
-				zap.S().Debugf("Consumer group error: %v", err)
+// filter applies regular expression filters to a list of topics and returns the filtered list.
+func filter(topics []string, regexes []*regexp.Regexp) []string {
+	filtered := make(map[string]bool)
+	for _, topic := range topics {
+		for _, re := range regexes {
+			if re.MatchString(topic) {
+				filtered[topic] = true
+				break
 			}
 		}
 	}
-}
-
-func shutdown(c chan bool) {
-	for {
-		select {
-		case c <- true:
-		default:
-			return
-		}
+	result := make([]string, 0, len(filtered))
+	for topic := range filtered {
+		result = append(result, topic)
 	}
+	zap.S().Debugf("Filtered topics: %v to %v", topics, result)
+	slices.Sort(result)
+	return result
 }
