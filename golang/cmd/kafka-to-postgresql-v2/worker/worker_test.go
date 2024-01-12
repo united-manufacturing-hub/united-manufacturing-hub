88d9f0ff7362ad5624edcb632090712fcf367483
Matteo Danelon <md@umh.app>
2024-01-12 13:53:53 +0100

    Add tests for parsing historian payload
    Signed-off-by: Matteo Danelon <md@umh.app>

--- golang/cmd/kafka-to-postgresql-v2/worker/worker_test.go
+++ golang/cmd/kafka-to-postgresql-v2/worker/worker_test.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/united-manufacturing-hub/Sarama-Kafka-Wrapper-2/pkg/kafka/shared"
+	sharedStructs "github.com/united-manufacturing-hub/united-manufacturing-hub/cmd/kafka-to-postgresql-v2/shared"
 	"math/rand"
 	"strings"
 	"testing"
@@ -28,9 +29,10 @@ func TestRecreateTopic(t *testing.T) {
 		"umh.v1.Che-rnobyl.Auxiliary.Building2.WaterSupply.MainValve1.ValveID-1234._historian.FlowRate",
 		"umh.v1.Che_rnobyl.Auxiliary.Building2.WaterSupply.MainValve1.ValveID-1234._historian.FlowRate",
 		"umh.v1.Che_rnobyl.Auxiliary.Building2.WaterSupply.MainValve1.ValveID-1234._analytics.FlowRate",
+		"umh.v1.Che_rnobyl.Auxiliary.Building2.WaterSupply.MainValve1.ValveID-1234._analytics.FlowRate.X",
+		"umh.v1.Che_rnobyl.Auxiliary.Building2.WaterSupply.MainValve1.ValveID-1234._analytics.FlowRate.X.Y.Z",
 	}
 	invalid := []string{
-		"umh.v1.Che_rnobyl.Auxiliary.Building2.WaterSupply.MainValve1.ValveID-1234._analytics.FlowRate.X",
 		"umh.v1.Chernobyl.MainSite",
 		"umh.v1.Chernobyl.Security",
 		"umh.v1.Chernobyl.VisitorCenter",
@@ -96,5 +98,122 @@ func TestRecreateTopic(t *testing.T) {
 		}
 		_, err := recreateTopic(&msg)
 		assert.NoError(t, err, "modified topic %s with key %s failed to parse", modifiedTopic, key)
+	}
+}
+func TestParseHistorianPayload(t *testing.T) {
+	sV := "this is a string"
+	var iV float32 = 1
+	var fV float32 = 1.5
+	var bV float32 = 1.0
+	testCases := []struct {
+		name     string
+		input    []byte
+		tag      string
+		expected []sharedStructs.Value
+	}{
+		{
+			name: "String value",
+			input: []byte(`{
+				"timestamp_ms": 12345,
+				"stringValue": "this is a string"
+			}`),
+			tag: "tag1",
+			expected: []sharedStructs.Value{
+				{
+					Name:        "tag1.stringValue",
+					StringValue: &sV,
+					IsNumeric:   false,
+				},
+			},
+		},
+		{
+			name: "Int value",
+			input: []byte(`{
+				"timestamp_ms": 12345,
+				"intValue": 1
+			}`),
+			tag: "tag2",
+			expected: []sharedStructs.Value{
+				{
+					Name:         "tag2.intValue",
+					NumericValue: &iV,
+					IsNumeric:    true,
+				},
+			},
+		},
+		{
+			name: "Float value",
+			input: []byte(`{
+				"timestamp_ms": 12345,
+				"floatValue": 1.5
+			}`),
+			tag: "tag3",
+			expected: []sharedStructs.Value{
+				{
+					Name:         "tag3.floatValue",
+					NumericValue: &fV,
+					IsNumeric:    true,
+				},
+			},
+		},
+		{
+			name: "Bool value",
+			input: []byte(`{
+				"timestamp_ms": 12345,
+				"boolValue": true
+			}`),
+			tag: "tag4",
+			expected: []sharedStructs.Value{
+				{
+					Name:         "tag4.boolValue",
+					NumericValue: &bV,
+					IsNumeric:    true,
+				},
+			},
+		},
+		{
+			name: "Nested struct value",
+			input: []byte(`{
+				"timestamp_ms": 12345,
+				"structValue": {
+					"stringValue": "this is a string",
+					"intValue": 1,
+					"floatValue": 1.5,
+					"boolValue": true
+				}
+			}`),
+			tag: "tag5",
+			expected: []sharedStructs.Value{
+				{
+					Name:        "tag5.structValue.stringValue",
+					StringValue: &sV,
+					IsNumeric:   false,
+				},
+				{
+					Name:         "tag5.structValue.intValue",
+					NumericValue: &iV,
+					IsNumeric:    true,
+				},
+				{
+					Name:         "tag5.structValue.floatValue",
+					NumericValue: &fV,
+					IsNumeric:    true,
+				},
+				{
+					Name:         "tag5.structValue.boolValue",
+					NumericValue: &bV,
+					IsNumeric:    true,
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			values, timestampMs, err := parseHistorianPayload(tc.input, tc.tag)
+			assert.NoError(t, err, "unexpected error")
+			assert.Equal(t, tc.expected, values, "unexpected values. want %+v, got %+v", tc.expected, values)
+			assert.Equal(t, int64(12345), timestampMs, "unexpected timestamp. want %d, got %d", 12345, timestampMs)
+		})
 	}
 }
