06a89ea4d37ad091450dfe0277fd7f47d22d13cf
Matteo Danelon <md@umh.app>
2024-01-18 14:10:11 +0100

    Update wait-for-services.sh script to include timeout
    Signed-off-by: Matteo Danelon <md@umh.app>

--- test/integration/kafka-to-postgresql-v2/wait-for-services.sh
+++ test/integration/kafka-to-postgresql-v2/wait-for-services.sh
@@ -2,14 +2,26 @@
 
 # Wait for TimescaleDB to be ready
 echo "Waiting for TimescaleDB to start..."
-until docker exec timescaledb pg_isready -U postgres; do
-  sleep 1
+timeout=0
+until ((timeout == 10)) || docker exec timescaledb pg_isready -U postgres; do
+    sleep 1
+    ((timeout++))
 done
+if ((timeout == 10)); then
+    echo "Timed out waiting for TimescaleDB to start."
+    exit 1
+fi
 echo "TimescaleDB is ready."
 
 # Check if RedPanda is up
 echo "Checking if RedPanda is up..."
-until docker exec redpanda bash -c "rpk cluster health | grep -E 'Healthy:.+true' || exit 1"; do
+timeout=0
+until ((timeout == 10)) || docker exec redpanda bash -c "rpk cluster health | grep -E 'Healthy:.+true'"; do
     sleep 1
+    ((timeout++))
 done
+if ((timeout == 10)); then
+    echo "Timed out waiting for RedPanda to start."
+    exit 1
+fi
 echo "RedPanda is up."
