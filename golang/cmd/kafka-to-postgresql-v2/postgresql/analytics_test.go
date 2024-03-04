c5d8e83c10889eafc61ed645f806299eab33f8f0
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2024-03-04 13:14:28 +0000

    feat: mock test for work-order/create

--- golang/cmd/kafka-to-postgresql-v2/postgresql/analytics_test.go
+++ golang/cmd/kafka-to-postgresql-v2/postgresql/analytics_test.go
@@ -1,1 +1,57 @@
 package postgresql
+
+import (
+	"github.com/jackc/pgx/v5"
+	"github.com/pashagolub/pgxmock/v3"
+	"github.com/stretchr/testify/assert"
+	sharedStructs "github.com/united-manufacturing-hub/united-manufacturing-hub/cmd/kafka-to-postgresql-v2/shared"
+	"testing"
+)
+
+func TestInsertWorkOrder(t *testing.T) {
+	c := CreateMockConnection(t)
+	defer c.db.Close()
+
+	t.Run("insert", func(t *testing.T) {
+		msg := sharedStructs.WorkOrderCreateMessage{
+			ExternalWorkOrderId: "#1274",
+			Product: sharedStructs.WorkOrderCreateMessageProduct{
+				ExternalProductId: "test",
+				CycleTimeMs:       120,
+			},
+			Quantity:        0,
+			Status:          0,
+			StartTimeUnixMs: 0,
+			EndTimeUnixMs:   0,
+		}
+		topic := sharedStructs.TopicDetails{
+			Enterprise: "umh",
+			Tag:        "work-order.create",
+		}
+
+		// Cast c.db to pgxmock to access the underlying mock
+		mock, ok := c.db.(pgxmock.PgxPoolIface)
+		assert.True(t, ok)
+		// Expect Query from GetOrInsertAsset
+		mock.ExpectQuery(`SELECT id FROM asset WHERE enterprise = \$1 AND site = \$2 AND area = \$3 AND line = \$4 AND workcell = \$5 AND origin_id = \$6`).
+			WithArgs("umh", "", "", "", "", "").
+			WillReturnRows(mock.NewRows([]string{"id"}).AddRow(1))
+
+		// Expect Query from GetOrInsertProduct
+		mock.ExpectQuery(`SELECT productTypeId FROM product_types WHERE externalProductTypeId = \$1 AND assetId = \$2`).
+			WithArgs("test", 1).
+			WillReturnRows(mock.NewRows([]string{"productTypeId"}).AddRow(1))
+
+		// Expect Exec from InsertWorkOrderCreate
+		mock.ExpectBeginTx(pgx.TxOptions{})
+		mock.ExpectExec(`
+		INSERT INTO work_orders \(externalWorkOrderId, assetId, productTypeId, quantity, status, to_timestamp\(\$6 \/ 1000\), to_timestamp\(\$7 \/ 1000\)\)
+		VALUES \(\$1, \$2, \$3, \$4, \$5, \$6, \$7\)
+	`).WithArgs("#1274", 1, 1, uint64(0), int(0), uint64(0), uint64(0)).
+			WillReturnResult(pgxmock.NewResult("INSERT", 1))
+		mock.ExpectCommit()
+
+		err := c.InsertWorkOrderCreate(&msg, &topic)
+		assert.NoError(t, err)
+	})
+}
--- golang/cmd/kafka-to-postgresql-v2/worker/analytics.go
+++ golang/cmd/kafka-to-postgresql-v2/worker/analytics.go
@@ -6,55 +6,55 @@ import (
 	"github.com/united-manufacturing-hub/united-manufacturing-hub/cmd/kafka-to-postgresql-v2/shared"
 )
 
-func parseWorkOrderCreate(value []byte) (shared.WorkOrderCreateMessage, error) {
+func parseWorkOrderCreate(value []byte) (*shared.WorkOrderCreateMessage, error) {
 	// Try parse to WorkOrderCreateMessage
 	var message shared.WorkOrderCreateMessage
 	err := json.Unmarshal(value, &message)
 
 	// Validate that ExternalWorkOrderId, Product.ExternalProductId & Quantity are set
 	if message.ExternalWorkOrderId == "" {
-		return message, errors.New("externalWorkOrderId is required")
+		return nil, errors.New("externalWorkOrderId is required")
 	}
 	if message.Product.ExternalProductId == "" {
-		return message, errors.New("product.externalProductId is required")
+		return nil, errors.New("product.externalProductId is required")
 	}
 	if message.Quantity == 0 {
-		return message, errors.New("quantity is required")
+		return nil, errors.New("quantity is required")
 	}
 	if int(message.Status) > int(shared.Completed) {
-		return message, errors.New("status must be 0, 1 or 2")
+		return nil, errors.New("status must be 0, 1 or 2")
 	}
 	// Status falls back to zero, if not set
 
-	return message, err
+	return &message, err
 }
 
-func parseWorkOrderStart(value []byte) (shared.WorkOrderStartMessage, error) {
+func parseWorkOrderStart(value []byte) (*shared.WorkOrderStartMessage, error) {
 	// Try parse to WorkOrderStartMessage
 	var message shared.WorkOrderStartMessage
 	err := json.Unmarshal(value, &message)
 
 	// Validate that ExternalWorkOrderId & StartTimeUnixMs are set
 	if message.ExternalWorkOrderId == "" {
-		return message, errors.New("externalWorkOrderId is required")
+		return nil, errors.New("externalWorkOrderId is required")
 	}
 	if message.StartTimeUnixMs == 0 {
-		return message, errors.New("start_time_unix_ms is required")
+		return nil, errors.New("start_time_unix_ms is required")
 	}
-	return message, err
+	return &message, err
 }
 
-func parseWorkOrderStop(value []byte) (shared.WorkOrderStopMessage, error) {
+func parseWorkOrderStop(value []byte) (*shared.WorkOrderStopMessage, error) {
 	// Try parse to WorkOrderStopMessage
 	var message shared.WorkOrderStopMessage
 	err := json.Unmarshal(value, &message)
 
 	// Validate that ExternalWorkOrderId & EndTimeUnixMs are set
 	if message.ExternalWorkOrderId == "" {
-		return message, errors.New("externalWorkOrderId is required")
+		return nil, errors.New("externalWorkOrderId is required")
 	}
 	if message.EndTimeUnixMs == 0 {
-		return message, errors.New("end_time_unix_ms is required")
+		return nil, errors.New("end_time_unix_ms is required")
 	}
-	return message, err
+	return &message, err
 }
