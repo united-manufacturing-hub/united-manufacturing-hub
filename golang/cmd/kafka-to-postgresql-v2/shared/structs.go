19308d514bfaaa6312aaf34b36bc3f9c4ca8cd04
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2024-03-04 09:00:40 +0000

    feat: added work-order message structs

--- golang/cmd/kafka-to-postgresql-v2/shared/structs.go
+++ golang/cmd/kafka-to-postgresql-v2/shared/structs.go
@@ -3,6 +3,7 @@ package shared
 const (
 	DbTagSeparator = "$"
 )
+
 type TopicDetails struct {
 	Enterprise     string
 	Site           string
@@ -14,9 +15,39 @@ type TopicDetails struct {
 	Tag            string
 }
 
-type Value struct {
+type HistorianValue struct {
 	NumericValue *float32
 	StringValue  *string
 	IsNumeric    bool
 	Name         string
+}
+
+type Status int
+
+const (
+	Planned Status = iota
+	InProgress
+	Completed
+)
+
+type WorkOrderCreateMessage struct {
+	ExternalWorkOrderId string `json:"externalWorkOrderId"`
+	Product             struct {
+		ExternalProductId string `json:"externalProductId"`
+		CycleTimeMs       int    `json:"cycleTimeMs"`
+	} `json:"product"`
+	Quantity        uint64 `json:"quantity"`
+	Status          Status `json:"status"`
+	StartTimeUnixMs int64  `json:"startTimeUnixMs"`
+	EndTimeUnixMs   int64  `json:"endTimeUnixMs"`
+}
+
+type WorkOrderStartMessage struct {
+	ExternalWorkOrderId string `json:"externalWorkOrderId"`
+	StartTimeUnixMs     int64  `json:"startTimeUnixMs"`
+}
+
+type WorkOrderStopMessage struct {
+	ExternalWorkOrderId string `json:"externalWorkOrderId"`
+	EndTimeUnixMs       int64  `json:"endTimeUnixMs"`
 }
