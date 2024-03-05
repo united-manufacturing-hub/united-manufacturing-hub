7ed85461bf19fb7179686c255fec79b757e44b06
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2024-03-05 14:56:12 +0000

    fix: explicit cast

--- golang/cmd/kafka-to-postgresql-v2/postgresql/analytics_test.go
+++ golang/cmd/kafka-to-postgresql-v2/postgresql/analytics_test.go
@@ -50,7 +50,7 @@ func TestWorkOrder(t *testing.T) {
 		// Expect Exec from InsertWorkOrderCreate
 		mock.ExpectBeginTx(pgx.TxOptions{})
 		mock.ExpectExec(`
-		INSERT INTO work_order\(external_work_order_id, asset_id, product_type_id, quantity, status, start_time, end_time\) VALUES \(\$1, \$2, \$3, \$4, \$5, CASE WHEN \$6 IS NOT NULL THEN to_timestamp\(\$6/1000\) END, CASE WHEN \$7 IS NOT NULL THEN to_timestamp\(\$7/1000\) END\)
+		INSERT INTO work_order\(external_work_order_id, asset_id, product_type_id, quantity, status, start_time, end_time\) VALUES \(\$1, \$2, \$3, \$4, \$5, CASE WHEN \$6 IS NOT NULL THEN to_timestamp\(\$6/1000\) ELSE NULL END::timestamptz, CASE WHEN \$7 IS NOT NULL THEN to_timestamp\(\$7/1000\) ELSE NULL END::timestamptz\)
 	`).WithArgs("#1274", 1, 1, 0, 0, helper.Uint64PtrToNullInt64(helper.IntToUint64Ptr(0)), helper.Uint64PtrToNullInt64(helper.IntToUint64Ptr(0))).
 			WillReturnResult(pgxmock.NewResult("INSERT", 1))
 		mock.ExpectCommit()
@@ -148,7 +148,7 @@ func TestProduct(t *testing.T) {
 		// Expect Exec from InsertProductAdd
 		mock.ExpectBeginTx(pgx.TxOptions{})
 		mock.ExpectExec(`INSERT INTO product\(external_product_type_id, product_batch_id, asset_id, start_time, end_time, quantity, bad_quantity\)
-		VALUES \( \$1, \$2, \$3, CASE WHEN \$4 IS NOT NULL THEN to_timestamp\(\$4\/1000\) END, to_timestamp\(\$5\/1000\), \$6, \$7 \)`).
+		VALUES \( \$1, \$2, \$3, CASE WHEN \$4 IS NOT NULL THEN to_timestamp\(\$4\/1000\) ELSE NULL END::timestamptz, to_timestamp\(\$5\/1000\), \$6, \$7 \)`).
 			WithArgs(1, helper.StringToNullString("0000-1234"), 1, helper.Uint64PtrToNullInt64(helper.IntToUint64Ptr(0)), uint64(10), 512, helper.Uint64PtrToNullInt64(helper.IntToUint64Ptr(0))).
 			WillReturnResult(pgxmock.NewResult("INSERT", 1))
 		mock.ExpectCommit()
