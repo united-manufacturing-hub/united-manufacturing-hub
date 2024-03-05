5944a6e98f0dcb7c5c2837e03d6c1a9ab8cd7fda
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2024-03-05 13:57:55 +0000

    fix: remaining test

--- golang/cmd/kafka-to-postgresql-v2/worker/worker_test.go
+++ golang/cmd/kafka-to-postgresql-v2/worker/worker_test.go
@@ -325,8 +325,8 @@ func TestHandleParsing(t *testing.T) {
 	// Expect Exec from InsertWorkOrderCreate
 	mock.ExpectBeginTx(pgx.TxOptions{})
 	mock.ExpectExec(`
-		INSERT INTO work_order\(external_work_order_id, asset_id, product_type_id, quantity, status, start_time, end_time\) VALUES \(\$1, \$2, \$3, \$4, \$5, to_timestamp\(\$6\/1000\), to_timestamp\(\$7\/1000\)\)
-	`).WithArgs("#1244", 1, 1, uint64(100), int(0), uint64(0), uint64(0)).
+		INSERT INTO work_order\(external_work_order_id, asset_id, product_type_id, quantity, status, start_time, end_time\) VALUES \(\$1, \$2, \$3, \$4, \$5, CASE WHEN \$6 IS NOT NULL THEN to_timestamp\(\$6/1000\) END, CASE WHEN \$7 IS NOT NULL THEN to_timestamp\(\$7/1000\) END\)
+	`).WithArgs("#1244", 1, 1, 100, 0, helper.MatchInt64Ptr(nil), helper.MatchInt64Ptr(nil)).
 		WillReturnResult(pgxmock.NewResult("INSERT", 1))
 	mock.ExpectCommit()
 
