3708a9d8e452d81f9ac0bb4e464eb8a8a34c4b6c
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2024-03-05 14:14:16 +0000

    fix: better nil handling

--- golang/cmd/kafka-to-postgresql-v2/helper/helper.go
+++ golang/cmd/kafka-to-postgresql-v2/helper/helper.go
@@ -1,10 +1,9 @@
 package helper
 
 import (
-	"github.com/pashagolub/pgxmock/v3"
+	"database/sql"
 	"github.com/united-manufacturing-hub/umh-utils/env"
 	"github.com/united-manufacturing-hub/umh-utils/logger"
-	"go.uber.org/zap"
 )
 
 func InitLogging() {
@@ -16,17 +15,17 @@ func InitTestLogging() {
 	_ = logger.New("DEVELOPMENT")
 }
 
-// Uint64PtrToInt64Ptr
+// Uint64PtrToNullInt64
 // Function to convert *uint64 to a value that can be inserted into the DB.
-// It returns an interface{} which can either be a nil (representing SQL NULL)
-// or an int64 value (if conversion is within range and not nil).
-func Uint64PtrToInt64Ptr(val *uint64) *int64 {
+// It returns an sql.NullInt64, which is a struct that can hold an int64 value and a flag indicating if the value is valid.
+func Uint64PtrToNullInt64(val *uint64) sql.NullInt64 {
 	if val == nil {
-		return nil
+		return sql.NullInt64{Valid: false}
 	}
 	// Note: This assumes the uint64 value can safely be converted to an int64.
 	v := int64(*val)
-	return &v
+
+	return sql.NullInt64{Valid: true, Int64: v}
 }
 
 func IntToUint64Ptr(val int) *uint64 {
@@ -39,102 +38,22 @@ func IntToInt64Ptr(val int) *int64 {
 	return &v
 }
 
+// StringToPtr you probably don't need this outside of tests
+// Checkout StringPtrToNullString for a more useful function
 func StringToPtr(val string) *string {
 	return &val
 }
 
-type uint64PtrMatcher struct {
-	expected *uint64
+func StringPtrToNullString(val *string) sql.NullString {
+	if val == nil {
+		return sql.NullString{Valid: false}
+	}
+	return sql.NullString{Valid: true, String: *val}
 }
 
-func (m uint64PtrMatcher) Match(v interface{}) bool {
-	zap.S().Debugf("Comparing %v to %v", m.expected, v)
-	// Type assert the value to a pointer to uint64.
-	ptr, ok := v.(*uint64)
-	if !ok {
-		zap.S().Debugf("Expected *uint64, got %T", v)
-		return false
+func StringToNullString(val string) sql.NullString {
+	if val == "" {
+		return sql.NullString{Valid: false}
 	}
-	// If both are nil, consider it a match.
-	if m.expected == nil && ptr == nil {
-		zap.S().Debugf("Matched nils")
-		return true
-	}
-	// If one is nil but not the other, it's not a match.
-	if m.expected == nil || ptr == nil {
-		zap.S().Debugf("One is nil, the other is not")
-		return false
-	}
-	// Compare the values pointed to.
-	zap.S().Debugf("%d == %d", *m.expected, *ptr)
-	return *m.expected == *ptr
-}
-
-// Helper function to create a new uint64PtrMatcher.
-func MatchUint64Ptr(expected *uint64) pgxmock.Argument {
-	return uint64PtrMatcher{expected}
-}
-
-type int64PtrMatcher struct {
-	expected *int64
-}
-
-func (m int64PtrMatcher) Match(v interface{}) bool {
-	zap.S().Debugf("Comparing %v to %v", m.expected, v)
-	// Type assert the value to a pointer to uint64.
-	ptr, ok := v.(*int64)
-	if !ok {
-		zap.S().Debugf("Expected *uint64, got %T", v)
-		return false
-	}
-	// If both are nil, consider it a match.
-	if m.expected == nil && ptr == nil {
-		zap.S().Debugf("Matched nils")
-		return true
-	}
-	// If one is nil but not the other, it's not a match.
-	if m.expected == nil || ptr == nil {
-		zap.S().Debugf("One is nil, the other is not")
-		return false
-	}
-	// Compare the values pointed to.
-	zap.S().Debugf("%d == %d", *m.expected, *ptr)
-	return *m.expected == *ptr
-}
-
-// Helper function to create a new int64PtrMatcher.
-func MatchInt64Ptr(expected *int64) pgxmock.Argument {
-	return int64PtrMatcher{expected}
-}
-
-type stringPtrMatcher struct {
-	expected *string
-}
-
-func (m stringPtrMatcher) Match(v interface{}) bool {
-	zap.S().Debugf("Comparing %v to %v", m.expected, v)
-	// Type assert the value to a pointer to ustring.
-	ptr, ok := v.(*string)
-	if !ok {
-		zap.S().Debugf("Expected *ustring, got %T", v)
-		return false
-	}
-	// If both are nil, consider it a match.
-	if m.expected == nil && ptr == nil {
-		zap.S().Debugf("Matched nils")
-		return true
-	}
-	// If one is nil but not the other, it's not a match.
-	if m.expected == nil || ptr == nil {
-		zap.S().Debugf("One is nil, the other is not")
-		return false
-	}
-	// Compare the values pointed to.
-	zap.S().Debugf("%d == %d", *m.expected, *ptr)
-	return *m.expected == *ptr
-}
-
-// Helper function to create a new stringPtrMatcher.
-func MatchstringPtr(expected *string) pgxmock.Argument {
-	return stringPtrMatcher{expected}
+	return sql.NullString{Valid: true, String: val}
 }
