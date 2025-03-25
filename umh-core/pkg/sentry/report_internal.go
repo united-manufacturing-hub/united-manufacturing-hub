70ed3a8f1734da4ee5826e863bd3a3935afc0b82
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2025-03-25 17:48:05 +0100

    feat: added logger

--- umh-core/pkg/sentry/report_internal.go
+++ umh-core/pkg/sentry/report_internal.go
@@ -1,14 +1,13 @@
 package sentry
 
 import (
-	"errors"
-	"fmt"
 	"runtime/debug"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/getsentry/sentry-go"
+	"go.uber.org/zap"
 )
 
 func getDash(inp string) string {
@@ -16,50 +15,25 @@ func getDash(inp string) string {
 	return strings.Repeat("=", len(inp))
 }
 
-// Fatal error handler
-func reportFatal(err string) {
-	var sb strings.Builder
-	sb.WriteString("\n")
-	sb.WriteString(getDash(err))
-	sb.WriteString("\n")
-	sb.WriteString(err)
-	sb.WriteString("\n")
-	sb.WriteString("Please contact our customer support")
-	sb.WriteString("\n")
-	sb.WriteString(getDash(err))
-	sb.WriteString("\n")
-	sb.WriteString("Stack trace:")
-	sb.WriteString(string(debug.Stack()))
-	sb.WriteString("\n")
-	fmt.Println(sb.String())
-
-	event := createSentryEvent(sentry.LevelFatal, errors.New(err))
+// reportFatal sends a fatal error to Sentry, including a stack trace and a message
+// Afterwards it will report the error to the logger and panic
+func reportFatal(err error, log *zap.SugaredLogger) {
+	event := createSentryEvent(sentry.LevelFatal, err)
 	sendSentryEvent(event)
 
 	sentry.Flush(time.Second * 5)
-	panic(err)
-}
-
-var warningLastSent time.Time = time.Now().Add(-time.Hour * 24)
-var warningLastSentMutex sync.Mutex = sync.Mutex{}
-
-func reportWarning(err string) {
-	warningLastSentMutex.Lock()
-	defer warningLastSentMutex.Unlock()
-
-	if time.Since(warningLastSent) < time.Hour*2 {
-		return
-	}
-
-	event := createSentryEvent(sentry.LevelWarning, errors.New(err))
-	sendSentryEvent(event)
-	warningLastSent = time.Now()
+	log.Error("The UMH-Core has encountered a fatal error and will now terminate. Please contact our customer support.")
+	log.Errorf("Error: %s", err)
+	log.Errorf("Stack trace: %s", string(debug.Stack()))
+	log.Panic("Fatal error")
 }
 
 var errorLastSent time.Time = time.Now().Add(-time.Hour * 24)
 var errorLastSentMutex sync.Mutex = sync.Mutex{}
 
-func reportError(err string) {
+// reportError sends an error to Sentry, including a stack trace and a message
+// Afterwards it will report the error to the logger
+func reportError(err error, log *zap.SugaredLogger) {
 	errorLastSentMutex.Lock()
 	defer errorLastSentMutex.Unlock()
 
@@ -67,8 +41,27 @@ func reportError(err string) {
 		return
 	}
 
-	event := createSentryEvent(sentry.LevelError, errors.New(err))
+	event := createSentryEvent(sentry.LevelError, err)
 	sendSentryEvent(event)
 	errorLastSent = time.Now()
+	log.Error(err)
+}
 
+var warningLastSent time.Time = time.Now().Add(-time.Hour * 24)
+var warningLastSentMutex sync.Mutex = sync.Mutex{}
+
+// reportWarning sends a warning to Sentry, including a stack trace and a message
+// Afterwards it will report the warning to the logger
+func reportWarning(err error, log *zap.SugaredLogger) {
+	warningLastSentMutex.Lock()
+	defer warningLastSentMutex.Unlock()
+
+	if time.Since(warningLastSent) < time.Hour*2 {
+		return
+	}
+
+	event := createSentryEvent(sentry.LevelWarning, err)
+	sendSentryEvent(event)
+	warningLastSent = time.Now()
+	log.Warn(err)
 }
