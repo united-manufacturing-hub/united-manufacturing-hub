97fa841a75a15e3c4385c155695516f85409f41b
Paul Herrmann <paul.herrmann@umh.app>
2025-04-07 12:10:08 +0000

    make context timeout for actions a constant

---
+++ umh-core/pkg/constants/actions.go
@@ -0,0 +1,23 @@
+// Copyright 2025 UMH Systems GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package constants
+
+import "time"
+
+const (
+	// this is the timeout for all actions
+	// they should not take any longer than 5 seconds, because they only read/write to the config file or check the system state
+	ActionTimeout = time.Second * 5
+)
