834b360a679d32c8162974469ae414dd9638c5c5
Jeremy Theocharis <jt@umh.app>
2025-04-10 16:13:02 +0200

    Ensure consistency in fsms for umh-core through interfaces (#1917)
    * made an action interface, with the goal of standardizing the code
    
    * removed part of hte code that were never intended to be merged
    
    * fix: merge conflicts
    
    * license header

---
+++ umh-core/pkg/fsm/interfaces.go
@@ -0,0 +1,39 @@
+// Copyright 2025 UMH Systems GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fsm
+
+import (
+	"context"
+
+	"github.com/united-manufacturing-hub/united-manufacturing-hub/umh-core/pkg/service/filesystem"
+)
+
+// FSMActions defines the standard lifecycle actions that all FSM instances should implement
+type FSMInstanceActions interface {
+	// CreateInstance initiates the creation of a managed instance
+	CreateInstance(ctx context.Context, filesystemService filesystem.Service) error
+
+	// RemoveInstance initiates the removal of a managed instance
+	RemoveInstance(ctx context.Context, filesystemService filesystem.Service) error
+
+	// StartInstance initiates the starting of a managed instance
+	StartInstance(ctx context.Context, filesystemService filesystem.Service) error
+
+	// StopInstance initiates the stopping of a managed instance
+	StopInstance(ctx context.Context, filesystemService filesystem.Service) error
+
+	// UpdateObservedStateOfInstance updates the observed state of the instance
+	UpdateObservedStateOfInstance(ctx context.Context, filesystemService filesystem.Service, tick uint64) error
+}
