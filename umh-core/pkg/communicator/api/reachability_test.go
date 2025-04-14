9bc05409446a53523262800ee469dbc5c8a9e71b
Jeremy Theocharis <jt@umh.app>
2025-04-14 12:04:50 +0200

    Eng 2774 get dataflowcomponent action v2 (#1933)
    * base
    
    * unit tests passing
    
    * added code comments
    
    * Add test for handling invalid flattened payload structure in DeployDataflowComponent
    
    * added docker on macos overwrite for disk
    
    * Revert "added docker on macos overwrite for disk"
    
    This reverts commit 7e0147c291a53e4a5f3aa656b9e290bb5f771682.
    
    * Enhance Docker test configuration and update watchdog logging. Added AUTH_TOKEN and LOGGING_LEVEL environment variables to the Docker test command in the Makefile. Commented out the heartbeat reporting logic in the watchdog to reduce console spam, while retaining the logging functionality for debugging purposes.
    
    * removed debug statement
    
    * savegame
    
    * more logger
    
    * savegame
    
    * fix: bug and added better documentation
    
    * removed mocks

--- umh-core/pkg/communicator/api/reachability_test.go
+++ umh-core/pkg/communicator/api/reachability_test.go
@@ -17,15 +17,24 @@ package api_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.uber.org/zap"
 
 	"github.com/united-manufacturing-hub/united-manufacturing-hub/umh-core/pkg/communicator/api"
+	"github.com/united-manufacturing-hub/united-manufacturing-hub/umh-core/pkg/logger"
 )
 
 var _ = Describe("Checking API reachability", func() {
+	const apiUrl = "https://management.umh.app/api"
+	var log *zap.SugaredLogger
+
+	BeforeEach(func() {
+		log = logger.For("reachability_test")
+	})
+
 	When("API is reachable", func() {
 		It("can be reached", func() {
 			Eventually(func() bool {
-				return api.CheckIfAPIIsReachable(false)
+				return api.CheckIfAPIIsReachable(false, apiUrl, log)
 			}, "5m", "1s").Should(BeTrue())
 		})
 	})
