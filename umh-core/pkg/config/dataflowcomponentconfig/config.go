cdf8ec8203164a5f914aede9ba4f52320ed64c4a
Paul Herrmann <paul.herrmann@umh.app>
2025-04-24 12:06:07 +0000

    remove unneeded nil check

--- umh-core/pkg/config/dataflowcomponentconfig/config.go
+++ umh-core/pkg/config/dataflowcomponentconfig/config.go
@@ -57,9 +57,6 @@ func (bc *BenthosConfig) ToBenthosServiceConfig() benthosserviceconfig.BenthosSe
 
 // GetBenthosServiceConfig converts the component config to a full BenthosServiceConfig
 func (c *DataFlowComponentConfig) GetBenthosServiceConfig() benthosserviceconfig.BenthosServiceConfig {
-	if c == nil {
-		return benthosserviceconfig.BenthosServiceConfig{}
-	}
 	return c.BenthosConfig.ToBenthosServiceConfig()
 }
 
