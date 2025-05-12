58f0af98617cbbb5b89b888b86db790f10ed7520
Paul Herrmann <paul.herrmann@umh.app>
2025-05-12 11:55:39 +0200

    typo

--- umh-core/pkg/communicator/pkg/generator/generator.go
+++ umh-core/pkg/communicator/pkg/generator/generator.go
@@ -135,7 +135,7 @@ func (s *StatusCollectorType) GenerateStatusMessage() *models.StatusMessage {
 					"action-delete-data-flow-component",
 					"action-edit-data-flow-component",
 					"action-get-logs",
-					"action-get-dataflowcomponent-metrics",
+					"action-get-data-flow-component-metrics",
 				},
 			},
 		},
