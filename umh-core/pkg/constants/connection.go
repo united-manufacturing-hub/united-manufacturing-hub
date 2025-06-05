7fd4de24d720e07f9c0e939a27ec67fe0b287601
Jeremy Theocharis <jt@umh.app>
2025-06-05 18:10:30 +0200

    fix: multiple bug fixes related to connection and nmap  (#2060)
    * fix: multiple bug fixes related to connection and nmap
    
    * refactor(fsm): remove redundant tick and loopStartTime parameters from UpdateObservedStateOfInstance
    
    Remove redundant tick and loopStartTime parameters from UpdateObservedStateOfInstance
    method signature across all FSM implementations. These values are already available
    as snapshot.Tick and snapshot.SnapshotTime in the SystemSnapshot parameter.
    
    Changes:
    - Update FSMInstanceActions interface to remove redundant parameters
    - Update all 11 FSM implementations (agent_monitor, benthos, benthos_monitor,
      container, connection, dataflowcomponent, nmap, redpanda, redpanda_monitor, s6)
    - Update all reconcileExternalChanges function signatures and calls
    - Replace parameter usage with snapshot.Tick and snapshot.SnapshotTime
    
    This improves API consistency by using a single source of truth (SystemSnapshot)
    and reduces parameter coupling across the FSM framework.
    
    * fix: code comments
    
    * added codecomment on dfcReadFound
    
    * fix: potential leftover from snapshot / tick sync
    
    * fix: remove unused tick variable in protocolconverter
    
    * minor changes to trigger ci/cd

--- umh-core/pkg/constants/connection.go
+++ umh-core/pkg/constants/connection.go
@@ -18,7 +18,7 @@ import "time"
 
 const (
 	// ConnectionExpectedMaxP95ExecutionTimePerInstance means that an instance will not reconcile if not 40ms are left
-	// Note: in the intergation test, we defined an alerting threshold of 80% of the max ticker time, which is 100ms
+	// Note: in the integration test, we defined an alerting threshold of 80% of the max ticker time, which is 100ms
 	// So by setting this to 40 ms, we can ensure that an instance will never start if it triggers the alerting threshold
 	ConnectionExpectedMaxP95ExecutionTimePerInstance = time.Millisecond * 40 // needs to be higher than NmapExpectedMaxP95ExecutionTimePerInstance
 )
