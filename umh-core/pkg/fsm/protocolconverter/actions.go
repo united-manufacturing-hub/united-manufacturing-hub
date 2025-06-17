479324a6f434e02e4cba4dffc8a5b3ceb3e26435
Paul Herrmann <paul.herrmann@umh.app>
2025-06-17 10:39:43 +0200

    add spec config to pc fsm observed state

--- umh-core/pkg/fsm/protocolconverter/actions.go
+++ umh-core/pkg/fsm/protocolconverter/actions.go
@@ -276,6 +276,7 @@ func (p *ProtocolConverterInstance) UpdateObservedStateOfInstance(ctx context.Co
 			if err != nil {
 				return fmt.Errorf("failed to update ProtocolConverter service configuration: %w", err)
 			}
+			p.baseFSMInstance.GetLogger().Debugf("config updated")
 
 			// UNIQUE BEHAVIOR: Re-evaluate DFC desired states after config changes
 			// This is different from other FSMs which set desired states once and don't change them.
@@ -284,6 +285,7 @@ func (p *ProtocolConverterInstance) UpdateObservedStateOfInstance(ctx context.Co
 			// 2. Empty DFCs should remain stopped, populated DFCs should be started
 			// 3. This ensures we don't start broken Benthos instances with empty configs
 			if p.baseFSMInstance.GetDesiredFSMState() == OperationalStateActive {
+				p.baseFSMInstance.GetLogger().Debugf("re-evaluating DFC desired states and will be active")
 				err := p.service.EvaluateDFCDesiredStates(p.baseFSMInstance.GetID(), "active")
 				if err != nil {
 					p.baseFSMInstance.GetLogger().Debugf("Failed to re-evaluate DFC states after config update: %v", err)
