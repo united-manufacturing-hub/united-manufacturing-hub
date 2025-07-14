eb76905859a90ceafdcef19e845a0622f9edda5b
Ferdinand Linnenberg <fl@umh.app>
2025-07-14 16:18:47 +0200

    feat: skeleton

--- umh-core/pkg/service/process_manager/ipm/internal.go
+++ umh-core/pkg/service/process_manager/ipm/internal.go
@@ -15,6 +15,9 @@
 package ipm
 
 import (
+	"errors"
+	"sync"
+
 	"github.com/united-manufacturing-hub/united-manufacturing-hub/umh-core/pkg/config/process_manager_serviceconfig"
 	"github.com/united-manufacturing-hub/united-manufacturing-hub/umh-core/pkg/service/filesystem"
 	"github.com/united-manufacturing-hub/united-manufacturing-hub/umh-core/pkg/service/process_manager/process_shared"
@@ -24,76 +27,125 @@ import (
 
 type ProcessManager struct {
 	Logger *zap.SugaredLogger
+	mu     sync.Mutex
+
+	services map[string]service
+}
+
+type service struct {
+	config process_manager_serviceconfig.ProcessManagerServiceConfig
 }
 
 const IPM_SERVICE_DIRECTORY = "/var/lib/umh/ipm"
 
-func (ProcessManager) Create(ctx context.Context, servicePath string, config process_manager_serviceconfig.ProcessManagerServiceConfig, fsService filesystem.Service) error {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) Create(ctx context.Context, servicePath string, config process_manager_serviceconfig.ProcessManagerServiceConfig, _ filesystem.Service) error {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Creating process manager service", zap.String("servicePath", servicePath), zap.Any("config", config))
+
+	return errors.New("not implemented")
 }
 
-func (ProcessManager) Remove(ctx context.Context, servicePath string, fsService filesystem.Service) error {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) Remove(ctx context.Context, servicePath string, _ filesystem.Service) error {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Removing process manager service", zap.String("servicePath", servicePath))
+
+	return errors.New("not implemented")
 }
 
-func (ProcessManager) Start(ctx context.Context, servicePath string, fsService filesystem.Service) error {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) Start(ctx context.Context, servicePath string, fsService filesystem.Service) error {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Starting process manager service", zap.String("servicePath", servicePath))
+
+	return errors.New("not implemented")
 }
 
-func (ProcessManager) Stop(ctx context.Context, servicePath string, fsService filesystem.Service) error {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) Stop(ctx context.Context, servicePath string, fsService filesystem.Service) error {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Stopping process manager service", zap.String("servicePath", servicePath))
+
+	return errors.New("not implemented")
 }
 
-func (ProcessManager) Restart(ctx context.Context, servicePath string, fsService filesystem.Service) error {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) Restart(ctx context.Context, servicePath string, fsService filesystem.Service) error {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Restarting process manager service", zap.String("servicePath", servicePath))
+
+	return errors.New("not implemented")
 }
 
-func (ProcessManager) Status(ctx context.Context, servicePath string, fsService filesystem.Service) (process_shared.ServiceInfo, error) {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) Status(ctx context.Context, servicePath string, fsService filesystem.Service) (process_shared.ServiceInfo, error) {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Getting status of process manager service", zap.String("servicePath", servicePath))
+
+	return process_shared.ServiceInfo{}, errors.New("not implemented")
 }
 
-func (ProcessManager) ExitHistory(ctx context.Context, superviseDir string, fsService filesystem.Service) ([]process_shared.ExitEvent, error) {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) ExitHistory(ctx context.Context, superviseDir string, fsService filesystem.Service) ([]process_shared.ExitEvent, error) {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Getting exit history of process manager service", zap.String("servicePath", superviseDir))
+
+	return []process_shared.ExitEvent{}, errors.New("not implemented")
 }
 
-func (ProcessManager) ServiceExists(ctx context.Context, servicePath string, fsService filesystem.Service) (bool, error) {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) ServiceExists(ctx context.Context, servicePath string, fsService filesystem.Service) (bool, error) {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Checking if process manager service exists", zap.String("servicePath", servicePath))
+
+	return false, errors.New("not implemented")
 }
 
-func (ProcessManager) GetConfig(ctx context.Context, servicePath string, fsService filesystem.Service) (process_manager_serviceconfig.ProcessManagerServiceConfig, error) {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) GetConfig(ctx context.Context, servicePath string, fsService filesystem.Service) (process_manager_serviceconfig.ProcessManagerServiceConfig, error) {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Getting config of process manager service", zap.String("servicePath", servicePath))
+
+	return process_manager_serviceconfig.ProcessManagerServiceConfig{}, errors.New("not implemented")
 }
 
-func (ProcessManager) CleanServiceDirectory(ctx context.Context, path string, fsService filesystem.Service) error {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) CleanServiceDirectory(ctx context.Context, path string, fsService filesystem.Service) error {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Cleaning process manager service directory", zap.String("servicePath", path))
+
+	return errors.New("not implemented")
 }
 
-func (ProcessManager) GetConfigFile(ctx context.Context, servicePath string, configFileName string, fsService filesystem.Service) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) GetConfigFile(ctx context.Context, servicePath string, configFileName string, fsService filesystem.Service) ([]byte, error) {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Getting config file of process manager service", zap.String("servicePath", servicePath), zap.String("configFileName", configFileName))
+
+	return []byte{}, errors.New("not implemented")
 }
 
-func (ProcessManager) ForceRemove(ctx context.Context, servicePath string, fsService filesystem.Service) error {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) ForceRemove(ctx context.Context, servicePath string, fsService filesystem.Service) error {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Force removing process manager service", zap.String("servicePath", servicePath))
+
+	return errors.New("not implemented")
 }
 
-func (ProcessManager) GetLogs(ctx context.Context, servicePath string, fsService filesystem.Service) ([]process_shared.LogEntry, error) {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) GetLogs(ctx context.Context, servicePath string, fsService filesystem.Service) ([]process_shared.LogEntry, error) {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Getting logs of process manager service", zap.String("servicePath", servicePath))
+
+	return []process_shared.LogEntry{}, errors.New("not implemented")
 }
 
-func (ProcessManager) EnsureSupervision(ctx context.Context, servicePath string, fsService filesystem.Service) (bool, error) {
-	//TODO implement me
-	panic("implement me")
+func (pm *ProcessManager) EnsureSupervision(ctx context.Context, servicePath string, fsService filesystem.Service) (bool, error) {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+	pm.Logger.Info("Ensuring supervision of process manager service", zap.String("servicePath", servicePath))
+
+	return false, errors.New("not implemented")
 }
