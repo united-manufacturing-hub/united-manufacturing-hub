2cc56cc9f0da1dd9260fc8e14e9fa8daebdac2d7
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2025-08-15 12:16:20 +0000

    reset

--- umh-core/pkg/communicator/models/topicbrowser/pb/topic_browser_data.pb.go
+++ umh-core/pkg/communicator/models/topicbrowser/pb/topic_browser_data.pb.go
@@ -39,13 +39,12 @@
 package topic_browser_plugin
 
 import (
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	wrapperspb "google.golang.org/protobuf/types/known/wrapperspb"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
