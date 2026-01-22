090ab099549d22b1ef74a58ebbf72202d5f036fd
Janik K. <janik@kndlive.de>
2026-01-22 18:18:51 +0100

    feat: work on fetcher to fetch based on subscribers
    Signed-off-by: Janik K. <janik@kndlive.de>

--- umh-core/pkg/gatekeeper/certificatehandler/interface.go
+++ umh-core/pkg/gatekeeper/certificatehandler/interface.go
@@ -1,10 +1,18 @@
 package certificatehandler
 
-import "crypto/x509"
+import (
+	"context"
+	"crypto/x509"
+)
 
 // Handler manages certificate caching and root CA storage.
 type Handler interface {
 	GetCertificate(email string) *x509.Certificate
 	GetIntermediateCerts(email string) []*x509.Certificate
 	GetRootCA() *x509.Certificate
+}
+
+// Fetcher fetches and stores certificates for users.
+type Fetcher interface {
+	RunForUser(ctx context.Context, email string)
 }
